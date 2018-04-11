c7745b64cb51e2529887a588110a7391df27c108
Crystal Shaw <crystal@wifi-131-179-29-181.host.ucla.edu>
2018-04-11 12:39:36 -0700

    Decrease variance of noise for Cij (to make dementia rates)

--- sex-demensia_sim_parA.R
+++ sex-demensia_sim_parA.R
@@ -18,7 +18,7 @@ pexp <- 0.49
 var0 <- 0.2   #Variance of random cognitive intercept
 var1 <- 0.005 #Variance of random cognitive slope
 cov <- 0.01   #Covariance of random intercept and random slope
-var3 <- 0.70  #Variance of noise for Cij (cognitive function for person i at time j)
+var3 <- 0.2  #Variance of noise for Cij (cognitive function for person i at time j)
 r1 <- 0.40    #Correlation between noise terms for Cij
 var4 <- 0.19  #Variance of measurement error of Cij
 
