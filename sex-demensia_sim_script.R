7bfc89b16224d5c4022477de5f4179f058a23d49
Crystal Shaw <crystal@wifi-131-179-29-181.host.ucla.edu>
2018-04-11 12:36:37 -0700

    Included "tidyverse" package Specified standard notation rounded to 3 decimal places Set seed Specified parameter file source

--- sex-demensia_sim_script.R
+++ sex-demensia_sim_script.R
@@ -1,1 +1,20 @@
+#---- Package loading, options, seed ----
+if (!require("pacman")) 
+  install.packages("pacman", repos='http://cran.us.r-project.org')
 
+p_load("tidyverse")
+
+#Using standard notation (as opposed to scientific), rounded to three 
+#decimal places
+options(scipen = 999)
+options(digits = 3)
+
+set.seed(10789)
+
+#---- Specify the parameter file ----
+source("sex-demensia_sim_parA.R")
+
+
+
+
+#Need a dataset with 3000 observations
