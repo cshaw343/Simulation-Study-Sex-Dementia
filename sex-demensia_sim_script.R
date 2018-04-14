5a9020119c4c97c5de2934969efba3fb5632e0c5
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-04-13 20:36:19 -0700

    Generate assessment timepoints

--- sex-demensia_sim_script.R
+++ sex-demensia_sim_script.R
@@ -14,14 +14,18 @@ set.seed(10789)
 #---- Specify the parameter file ----
 source("sex-demensia_sim_parA.R")
 
+#---- Generating assessment timepoints ---- 
+test_times <- seq(from = 0, to = int_time*num_tests, by = int_time)
+
 #---- The simulation function ----
 sex_dem_sim <- function(){
   #Generating the data
-  #Creating IDs, baseline age
+  #Generating IDs, baseline age, sex, U
   #Creating centered ages
   obs <- tibble("id" = seq(from = 1, to = num_obs, by = 1), 
                 "age0" = age0,
-                "sex" = rbinom(num_obs, size = 1, prob = psex)) %>% 
+                "sex" = rbinom(num_obs, size = 1, prob = psex), 
+                "U" = rnorm(num_obs, mean = 0, sd = 1)) %>% 
     mutate("age_c" = age0 - mean(age0))
   return(obs)
 }
