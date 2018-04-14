dbf212fb3e6dde2c60abe1713f037b4b78653628
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-04-13 20:10:54 -0700

    Simulation function up to generating ids, ages, and centered ages

--- sex-demensia_sim_script.R
+++ sex-demensia_sim_script.R
@@ -14,7 +14,16 @@ set.seed(10789)
 #---- Specify the parameter file ----
 source("sex-demensia_sim_parA.R")
 
+#---- The simulation function ----
+sex_dem_sim <- function(){
+  #Generating the data
+  #Creating IDs, baseline age
+  #Creating centered ages
+  obs <- tibble("id" = seq(from = 1, to = num_obs, by = 1), 
+                "age0" = age0,
+                "sex" = rbinom(num_obs, size = 1, prob = psex)) %>% 
+    mutate("age_c" = age0 - mean(age0))
+  return(obs)
+}
 
-
-
-#Need a dataset with 3000 observations
+sex_dem_sim()
