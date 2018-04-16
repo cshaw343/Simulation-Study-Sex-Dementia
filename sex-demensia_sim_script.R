677bdf504a444c47b9c54d472965cbf3ec110e48
Crystal Shaw <crystal@wifi-131-179-28-234.host.ucla.edu>
2018-04-16 10:55:15 -0700

    Created age variable names

--- sex-demensia_sim_script.R
+++ sex-demensia_sim_script.R
@@ -14,20 +14,30 @@ set.seed(10789)
 #---- Specify the parameter file ----
 source("sex-demensia_sim_parA.R")
 
-#---- Generating assessment timepoints ---- 
-test_times <- seq(from = 0, to = int_time*num_tests, by = int_time)
+#---- Generating assessment timepoint data ----
+
+visit_times <- seq(from = int_time, to = int_time*num_tests, by = int_time)
+
+#---- Generating variable names at assessment timepoints ----
+
+age_varnames <- vector(length = num_tests)
+for(i in 1:num_tests){
+  age_varnames[i] = paste("age", i, sep = "")
+}
 
 #---- The simulation function ----
 sex_dem_sim <- function(){
   #Generating the data
-  #Generating IDs, baseline age, sex, U
-  #Creating centered ages
+  #Generating IDs, sex, U
   obs <- tibble("id" = seq(from = 1, to = num_obs, by = 1), 
-                "age0" = age0,
                 "sex" = rbinom(num_obs, size = 1, prob = psex), 
-                "U" = rnorm(num_obs, mean = 0, sd = 1)) %>% 
-    mutate("age_c" = age0 - mean(age0))
-  return(obs)
+                "U" = rnorm(num_obs, mean = 0, sd = 1))
+  #Creating ages at each visit
+  ages <- 
+  #Creating centered ages
+  
+  
+  
 }
 
 sex_dem_sim()
