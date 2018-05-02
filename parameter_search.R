8a35e7d1df35accf0e8de5e8a0ab4214eea17c5e
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-05-01 20:36:06 -0700

    Added package loading and source files

--- parameter_search.R
+++ parameter_search.R
@@ -1,3 +1,15 @@
-#---- Specify source file ----
-source("sex-demensia_sim_script.R")
+#***************************************************************
+# Performs a parameter search using a specified function and 
+# specified file containing reference data
+#***************************************************************
 
+#---- Package Loading and Options ----
+if (!require("pacman")) 
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+
+p_load("tidyverse")
+
+#---- Specify source files ----
+source("sex-demensia_sim_parA.R")
+source("sex-demensia_sim_sanity_check.R")
+source("life_table2014.R")
