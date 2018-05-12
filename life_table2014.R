708167ba0e547cab311a9b1cacbc0d670a00a4f3
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-05-12 11:23:03 -0700

    Added data for age 45 to for hazard calcs

--- life_table2014.R
+++ life_table2014.R
@@ -21,21 +21,34 @@ cond_prob <- function(x){
   return(probs)
 }
 
+#---- Hazard Function ----
+haz <- function(age, lprob){
+  HZ <- vector(length = length(age))
+  for(i in 1:length(haz)){
+    
+  }
+}
+
+
 #---- Life Table Data ----
 #"Survivors" represents number surviving out of 100,000 born alive
-life <- tibble("Age" = seq(from = 50, to = 100, by = 5), 
-               "Survivors" = c(68429, 63947, 58079, 50560, 41090, 29729, 18298, 
-                               8683, 2941, 646, 67)) %>% 
-  mutate("CP" = cond_prob(Survivors))
+ages <- seq(from = 45, to = 100, by = 5) #Start at 45 for hazard calcs
 
-male_life <- tibble("MAge" = seq(from = 50, to = 100, by = 5), 
-                    "MSurvivors" = c(67553, 62965, 56917, 49218, 39668, 28316, 
-                                    17128, 7920, 2527, 556, 62)) %>% 
+life <- tibble("Age" = ages, 
+               "Survivors" = c(72036, 68429, 63947, 58079, 50560, 41090, 29729, 
+                               18298, 8683, 2941, 646, 67)) %>% 
+  mutate("Prob" = Survivors/100000,
+         "logProb" = log(Prob),
+         "CP" = cond_prob(Survivors))
+
+male_life <- tibble("MAge" = ages, 
+                    "MSurvivors" = c(71244, 67553, 62965, 56917, 49218, 39668, 
+                                     28316, 17128, 7920, 2527, 556, 62)) %>% 
   mutate("CP" = cond_prob(MSurvivors))
 
-female_life <- tibble("FAge" = seq(from = 50, to = 100, by = 5), 
-                      "FSurvivors" = c(69452, 65099, 59438, 52126, 42741, 31344, 
-                                      19613, 9515, 3314, 728, 72)) %>% 
+female_life <- tibble("FAge" = ages, 
+                      "FSurvivors" = c(72954, 69452, 65099, 59438, 52126, 42741, 
+                                       31344, 19613, 9515, 3314, 728, 72)) %>% 
   mutate("CP" = cond_prob(FSurvivors))
 
 #---- Hazard Function Calculations ----
