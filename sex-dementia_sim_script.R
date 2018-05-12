af4f18954aa6e0c3314aa7ada25a8ac343e9234c
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-05-12 13:19:46 -0700

    Updated survival function to incorporate parameter g1 as a vector

--- sex-dementia_sim_script.R
+++ sex-dementia_sim_script.R
@@ -78,7 +78,7 @@ survival <- function(obs, lambda){
     slope <- paste("slope", test_num, test_num + 1, sep = "")
     C <- paste("Ci", test_num, sep = "")
     Sij[j] = -log(obs[US])/
-      (lambda[j]*exp(g1*obs["sex"] + g2*obs[agec] + g3*obs["U"] + 
+      (lambda[j]*exp(g1[j]*obs["sex"] + g2*obs[agec] + g3*obs["U"] + 
                     g4*obs["sex"]*obs[agec] + g5*obs[slope] + 
                     g6*obs[C]))
   }
