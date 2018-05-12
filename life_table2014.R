a18850d8059a4423faa899ca5096e299300c624e
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-05-12 09:52:06 -0700

    Changed first line of conditional probability calc to be 1 instead of NA

--- life_table2014.R
+++ life_table2014.R
@@ -17,7 +17,7 @@ cond_prob <- function(x){
   for(i in 2:length(probs)){
     probs[i] = x[i]/x[i - 1]
   }
-  probs[1] <- NA
+  probs[1] <- 1
   return(probs)
 }
 
@@ -27,6 +27,7 @@ life <- tibble("Age" = seq(from = 50, to = 100, by = 5),
                "Survivors" = c(68429, 63947, 58079, 50560, 41090, 29729, 18298, 
                                8683, 2941, 646, 67)) %>% 
   mutate("CP" = cond_prob(Survivors))
+
 male_life <- tibble("MAge" = seq(from = 50, to = 100, by = 5), 
                     "MSurvivors" = c(67553, 62965, 56917, 49218, 39668, 28316, 
                                     17128, 7920, 2527, 556, 62)) %>% 
@@ -36,3 +37,6 @@ female_life <- tibble("FAge" = seq(from = 50, to = 100, by = 5),
                       "FSurvivors" = c(69452, 65099, 59438, 52126, 42741, 31344, 
                                       19613, 9515, 3314, 728, 72)) %>% 
   mutate("CP" = cond_prob(FSurvivors))
+
+#---- Hazard Function Calculations ----
+
