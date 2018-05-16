796643ab9ea7de74fd07c5d09e4024a936f3da6b
Crystal Shaw <crystal@wifi-131-179-28-170.host.ucla.edu>
2018-05-16 10:49:04 -0700

    Initial commit of calculations from Gross, et. al. paper

---
+++ dem_calcs(Gross, et. al).R
@@ -0,0 +1,19 @@
+#---- Package Loading and Options ----
+if (!require("pacman")) 
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+
+p_load("tidyverse")
+
+#Suppress warnings
+options(warn = -1)
+
+#Only consider the General cognitive performance data
+#Relevant individuals aged 70-85
+cog_decline <- tibble("WHICAP" = c(-0.4, -0.5, -0.4), 
+                      "SENAS" = c(-0.8, -0.4, -0.4), 
+                      "DUKE" = c(-0.4, -0.4, -0.45), 
+                      "NCODE" = c(0, 0, 0.1)) %>% map_df(.f = ~(.)/10)
+rownames(cog_decline) <- c("White", "Black", "Hispanic")
+
+#Average all data 
+avg_decline <- cog_decline %>% map_dbl(mean) %>% mean(.)
