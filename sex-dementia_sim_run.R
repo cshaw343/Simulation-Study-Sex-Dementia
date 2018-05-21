a2de403503ddf94606ce024fa92ddba2c81f1347
Crystal Shaw <crystal@wifi-131-179-28-158.host.ucla.edu>
2018-05-21 12:10:46 -0700

    Added code to test both methods of calculating slopes within intervals... they match =)

--- sex-dementia_sim_run.R
+++ sex-dementia_sim_run.R
@@ -1,14 +1,20 @@
 #---- Specify the simulation script ----
-source("sex-demensia_sim_script.R")
+source("sex-dementia_sim_script.R")
+source("sex-dementia_sim_script_testSlope.R")
 
 #---- Specify the parameter file ----
-source("sex-demensia_sim_parA.R")
+source("sex-dementia_sim_parA.R")
 
 #---- Running the simulation----
 #Storing the results of the simulation
+set.seed(0)
 sim_results <- replicate(1, sex_dem_sim())
+set.seed(0)
+sim_results_compare <- replicate(1, sex_dem_sim_test())
 
 #---- Looking at simulation results ----
 #Finding the mean Cij by sex across all simulations
-mean_Cij_sim <- as_tibble(do.call(rbind, sim_results)) %>% group_by(sex) %>% 
-  summarise_all(mean)
+mean_Cij_sim <- as_tibble(do.call(rbind, sim_results$mean_Cij)) 
+
+mean_Cij_sim_compare <- as_tibble(do.call(rbind, sim_results_compare)) %>% 
+  group_by(sex) %>% summarise_all(mean)
