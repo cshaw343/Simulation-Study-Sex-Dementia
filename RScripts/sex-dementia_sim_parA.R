b0d36ee8a80c9d28bbc8b6e9947fba060de6fa44
Crystal Shaw <crystal@wifi-131-179-29-244.host.ucla.edu>
2018-09-27 12:09:42 -0700

    Revised some comments

--- RScripts/sex-dementia_sim_parA.R
+++ RScripts/sex-dementia_sim_parA.R
@@ -1,11 +1,11 @@
-#***************************************************************
+#*******************************************************************************
 # Simulation Senario A
 # 25% cumulative incidence of mortality
 # Exposure = male gender
 # Based on US lifetables, sample is 51% F / 49% M at age 50
 # Remaining LE at age 50:  33.2 years for F and 29.6 years for M
-#this is a test change
-#***************************************************************
+# This is a test change
+#*******************************************************************************
 
 #---- Number of observations ----
 num_obs <- 100000
@@ -33,7 +33,7 @@ r1 <- 0.3     #Correlation between noise terms for Cij; this may need to be adju
 #var4 <- 0.19  #Variance of measurement error of Cij
 
 #---- Parameters for Cij ----
-#Knots placed at ages 70 and 85
+#Experimenting with knots placed at every age
 b00 <- 0      #Cognitive intercept for females
 b01 <- 0      #Effect of sex on cognitive intercept
 b02 <- -0.05  #Effect of age on cognitive intercept; Note: Everyone is the same age so there is no age effect
@@ -42,6 +42,7 @@ b03 <- 0      #Effect of U (unmeasured/underlying variable) on cognitive interce
 #First value is cognitive slopes, the remaining values are changes in cognitive slopes
 slopes <- c(rep(-0.01, 3), -0.015, -0.075, -0.0825, -0.155, -0.274, -1, 0)
 
+#This is replaced by the vector of slopes above
 # b10a <- 0     #Cognitive slope for females age 50-70
 # b10delta_b <- 0   #Change in cognitive slope for females age 70-85
 # b10delta_c <- 0   #Change in cognitive slope for females age 85+
@@ -63,11 +64,11 @@ g5 <- 0       #Effect of cognitive slope at time j on log hazard of death
 g6 <- 0       #Effect of cognitive function at time j on log hazard of death
 
 #---- Baseline hazard of death for unexposed ----
-#Computed in parameter_search.R script
+#Computed in lambda_search.R script
 #Based on 35x3000 = 105000 observations
 lambda <- c(0.0134, 0.0187, 0.0266, 0.0392, 0.0622, 0.0946, 0.1439, 0.2072, 
             0.2903, 0.4571)
 
 #---- Dementia Cut Point ----
-#Chosen using from dem_cut portion of parameter_search.R script
+#Chosen using demcut_search.R
 dem_cut <- -4.75
