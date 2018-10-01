f09345166ce55dc8f26a0618e56a9c9c10a4a4a4
Crystal Shaw <crystal@wifi-131-179-29-169.host.ucla.edu>
2018-10-01 10:58:38 -0700

    Moved survival times function into its own RScript

--- RScripts/sex-dementia_sim_script.R
+++ RScripts/sex-dementia_sim_script.R
@@ -13,24 +13,6 @@ set.seed(10789)
 #---- Generating assessment timepoint data ----
 visit_times <- seq(from = 0, to = int_time*num_tests, by = int_time)
 
-#---- Model for Survival Time ----
-survival <- function(obs, lambda){
-  #Calculate survival times for each interval
-  Sij <- vector(length = (length(visit_times) - 1))
-  for(j in 1:length(Sij)){
-    test_num = j - 1
-    US <- paste("U", test_num, test_num + 1, sep = "")
-    agec <- paste("age", test_num, "_c50", sep = "")
-    slope <- paste("slope", test_num, test_num + 1, sep = "")
-    C <- paste("Ci", test_num, sep = "")
-    Sij[j] = -log(obs[US])/
-      (lambda[j]*exp(g1[j]*obs["sex"] + g2*obs[agec] + g3*obs["U"] + 
-                       g4*obs["sex"]*obs[agec] + g5*obs[slope] + 
-                       g6*obs[C]))
-  }
-  return("Sij" = Sij)
-}
-
 #---- Function for computing dementia onset ----
 dem_onset <- function(obs){
   #Function we are trying to find the root of
