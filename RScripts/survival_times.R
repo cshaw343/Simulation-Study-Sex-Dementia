b43afe5c66377255987339a051a4326e46466105
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-10-03 17:31:19 -0700

    Changed to use the names in the variable names dataset to pick up the correct variable to compute survival time

--- RScripts/survival_times.R
+++ RScripts/survival_times.R
@@ -3,11 +3,10 @@ survival <- function(obs, lambda){
   #Calculate survival times for each interval
   Sij <- vector(length = (length(visit_times) - 1))
   for(j in 1:length(Sij)){
-    test_num = j - 1
-    US <- paste("USij", test_num, test_num + 1, sep = "")
-    agec <- paste("age", test_num, "_c50", sep = "")
-    slope <- paste("slope", test_num, test_num + 1, sep = "")
-    C <- paste("Ci", test_num, sep = "")
+    US <- variable_names$USij_varnames[j]
+    agec <- variable_names$agec_varnames[j]
+    slope <- variable_names$slopeij_varnames[j]
+    C <- variable_names$Cij_varnames[j]
     Sij[j] = -log(obs[US])/
       (lambda[j]*exp(g1[j]*obs["sex"] + g2*obs[agec] + g3*obs["U"] + 
                        g4*obs["sex"]*obs[agec] + g5*obs[slope] + 
@@ -15,3 +14,4 @@ survival <- function(obs, lambda){
   }
   return("Sij" = Sij)
 }
+
