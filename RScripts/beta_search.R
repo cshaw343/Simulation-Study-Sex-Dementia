ee328e89ddb57cc18994e71388eed996ebc5789f
Crystal Shaw <crystal@wifi-131-179-29-181.host.ucla.edu>
2018-10-15 10:13:01 -0700

    Initial commit of script that will search for parameter values of the logistic regression of dementia diagnosis on Cij, Fij, and sex

---
+++ RScripts/beta_search.R
@@ -0,0 +1,9 @@
+#*******************************************************************************
+# This is the function that performs the search for the beta values in the 
+# logistic regression of dementia diagnosis on Cij, Fij, and sex
+#*******************************************************************************
+
+
+find_betas <- function(){
+  
+}
