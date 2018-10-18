720778b02fe5ae024272dcd4ea2c0b8913d474f6
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-10-18 09:45:47 -0700

    Formatting changes

--- RScripts/sex-dementia_sim_run.R
+++ RScripts/sex-dementia_sim_run.R
@@ -14,17 +14,17 @@ source("RScripts/misc_custom_functions.R")    #Other functions needed
 use_cores <- detectCores() - 1                 #Use one less than number of available cores
 cl <- makeCluster(use_cores, type = "FORK")    #Make a cluster from your cores 
                                                #"FORKING" environment only works on Mac
-
+runs = 100
 #Storing the results of the simulation
 start <- proc.time()  #This times the code
-runs = 100
 sim_results <- parSapply(cl, 1:runs, sex_dem_sim)
 #stop the cluster
 stopCluster(cl)
 proc.time() - start
 
 #---- Running simulation on one core ----
-sim_results <- replicate(100, sex_dem_sim())
+runs = 100
+sim_results <- replicate(runs, sex_dem_sim())
 
 #---- Converting results to usable format ----
 results_mat <- matrix(nrow = runs*num_obs, ncol = nrow(sim_results))
@@ -38,37 +38,37 @@ results_mat %<>% as.data.frame() %>% set_colnames(dimnames(sim_results)[[1]])
   test_life_table <- 
   sim_results[c("sex", head(variable_names$deathij_varnames,-1)), ] 
 
-results_mat <- matrix(nrow = runs*num_obs, ncol = nrow(test_life_table))
-for(r in 1:nrow(test_life_table)){
-  results_mat[, r] <- unlist(test_life_table[r, ])
-}
-colnames(results_mat) <- c("sex", head(variable_names$deathij_varnames, -1))
-results_mat %<>% as.data.frame()
-
-#Want to look at unconditional and conditional probabilities of survival
-  #Unconditional Survival
-  death_intervals <- colnames(results_mat)[-1]
-  all_death_counts <- results_mat %>% dplyr::select(-one_of("sex")) %>% 
-    colSums() 
-  all_survival_counts <- num_obs*runs - death_counts
-  all_cp_survival <- cond_prob(all_survival_counts)
-  all_cp_survival[1] <- all_survival_counts[1]/(num_obs*runs)
-
-  #Female survival
-  num_females <- results_mat %>% filter(sex == 0) %>% nrow()
-  female_death_counts <- results_mat %>% filter(sex == 0) %>% 
-    dplyr::select(-one_of("sex")) %>% colSums()
-  female_survival_counts <- num_females - female_death_counts
-  female_cp_survival <- cond_prob(female_survival_counts)
-  female_cp_survival[1] <- female_survival_counts[1]/(num_females)
+  results_mat <- matrix(nrow = runs*num_obs, ncol = nrow(test_life_table))
+  for(r in 1:nrow(test_life_table)){
+    results_mat[, r] <- unlist(test_life_table[r, ])
+  }
+  colnames(results_mat) <- c("sex", head(variable_names$deathij_varnames, -1))
+  results_mat %<>% as.data.frame()
   
-  #Male survival
-  num_males <- results_mat %>% filter(sex == 1) %>% nrow()
-  male_death_counts <- results_mat %>% filter(sex == 1) %>% 
-    dplyr::select(-one_of("sex")) %>% colSums()
-  male_survival_counts <- num_males - male_death_counts
-  male_cp_survival <- cond_prob(male_survival_counts)
-  male_cp_survival[1] <- male_survival_counts[1]/(num_males)
+  #Want to look at unconditional and conditional probabilities of survival
+    #Unconditional Survival
+    death_intervals <- colnames(results_mat)[-1]
+    all_death_counts <- results_mat %>% dplyr::select(-one_of("sex")) %>% 
+      colSums() 
+    all_survival_counts <- num_obs*runs - death_counts
+    all_cp_survival <- cond_prob(all_survival_counts)
+    all_cp_survival[1] <- all_survival_counts[1]/(num_obs*runs)
+  
+    #Female survival
+    num_females <- results_mat %>% filter(sex == 0) %>% nrow()
+    female_death_counts <- results_mat %>% filter(sex == 0) %>% 
+      dplyr::select(-one_of("sex")) %>% colSums()
+    female_survival_counts <- num_females - female_death_counts
+    female_cp_survival <- cond_prob(female_survival_counts)
+    female_cp_survival[1] <- female_survival_counts[1]/(num_females)
+    
+    #Male survival
+    num_males <- results_mat %>% filter(sex == 1) %>% nrow()
+    male_death_counts <- results_mat %>% filter(sex == 1) %>% 
+      dplyr::select(-one_of("sex")) %>% colSums()
+    male_survival_counts <- num_males - male_death_counts
+    male_cp_survival <- cond_prob(male_survival_counts)
+    male_cp_survival[1] <- male_survival_counts[1]/(num_males)
   
 
 
