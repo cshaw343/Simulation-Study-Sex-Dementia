f373a2d0837e56635b7e27e1878a24f1bccb436f
Crystal Shaw <crystal@Crystals-MacBook-Pro-2.local>
2018-10-18 10:21:02 -0700

    Reorganize code and wrote the loop to perform optimization for all dem risk values

--- RScripts/beta_search.R
+++ RScripts/beta_search.R
@@ -11,43 +11,48 @@
 #---- Source Files ----
 source("RScripts/dementia_incidence2000-2013.R")
 
+#---- Function that we are trying to optimize ----
+dem_diag <- function(BETAS, 
+                     sex_i, Cij, Fij, 
+                     risk_match){
+  
+  model <- BETAS[1] + BETAS[2]*Cij + BETAS[3]*Fij + BETAS[4]*sex_i + 
+    BETAS[5]*Cij*Fij
+  pi_hat <- exp(model)/(1 + exp(model))
+  dem_diag <- rbernoulli(1, p = pi_hat)*1
+  risk <- sum(dem_diag)/length(dem_diag)
+  
+  return(abs(risk - risk_match))
+}
+
 #---- Function that looks for the betas ----
 find_betas <- function(obs, dem_risk){
-  beta_mat <- matrix(nrow = length(dem_risk), ncol = 5)
-  #Doing this for the first case-- will use loop later
-  sex_i <- obs$sex
-  Cij <- obs$Ci4 
-  Fij <- obs$Fi4
-  
-  #Don't consider the cases who have died by age 65
-  sex_i_nomiss <- sex_i[-which(is.na(Cij))]
-  Cij_nomiss <- Cij[-which(is.na(Cij))]
-  Fij_nomiss <- Fij[-which(is.na(Cij))]
-  #Test with values for B first
-  B_0 <- 0
-  B_1 <- -0.5
-  B_2 <- -0.5
-  B_3 <- -0.5
-  B_4 <- -0.25
-  
-  dem_diag <- function(BETAS, 
-                       sex_i, Cij, Fij, 
-                       risk_match){
+  beta_results <- matrix(nrow = length(dem_risk), ncol = 6)
+  start = 4 #represent starting age where we have data (age 70)
+  for(i in 1:nrow(beta_results)){
+    sex_i <- obs$sex
+    Cij <- obs[, variable_names$Cij_varnames[start + 1]] 
+    Fij <- obs[, variable_names$Fij_varnames[start + 1]] 
     
-    model <- BETAS[1] + BETAS[2]*Cij + BETAS[3]*Fij + BETAS[4]*sex_i + 
-      BETAS[5]*Cij*Fij
-    pi_hat <- exp(model)/(1 + exp(model))
-    dem_diag <- rbernoulli(1, p = pi_hat)*1
-    risk <- sum(dem_diag)/length(dem_diag)
+    sex_i_nomiss <- sex_i[-which(is.na(Cij))]
+    Cij_nomiss <- Cij[-which(is.na(Cij))]
+    Fij_nomiss <- Fij[-which(is.na(Cij))]
     
-    return(abs(risk - risk_match))
+    many_opts <- replicate(10, 
+                           {optim(par = rep(-0.05, 5), 
+                                  fn = dem_diag, 
+                                  sex_i = sex_i_nomiss, Cij = Cij_nomiss, 
+                                  Fij = Fij_nomiss, 
+                                  risk_match = dem_risk[[i]], 
+                                  upper = rep(0, 5), 
+                                  lower = rep(-1, 5))})
+    
+    mean_diff <- many_opts["value", ] %>% unlist() %>% mean()
+    mean_betas <- matrix(unlist(many_opts["par", ]), ncol = 5, byrow = TRUE) %>%
+      colMeans()
+    
+    beta_results[i, ] <- c(mean_betas, mean_diff)
+    
+    start = start + 1
   }
-  
-  optim(par = rep(-0.05, 5), 
-        fn = dem_diag, 
-        sex_i = sex_i_nomiss, Cij = Cij_nomiss, Fij = Fij_nomiss, 
-        risk_match = dem_risk[[1]],
-        upper = rep(0, 5), 
-        lower = rep(-0.5, 5))
-  
 }
