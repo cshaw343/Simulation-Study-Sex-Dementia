f986bef446813fe5ec20d83ee81dc0c235aae13c
Crystal Shaw <crystal@wifi-131-179-29-238.host.ucla.edu>
2018-10-19 10:45:32 -0700

    I realized that I wasn't computing the risk percentage in the optimizing function-- whoops!

--- RScripts/beta_search.R
+++ RScripts/beta_search.R
@@ -20,50 +20,62 @@ dem_diag <- function(BETAS,
     BETAS[5]*Cij*Fij
   pi_hat <- exp(model)/(1 + exp(model))
   dem_diag <- rbernoulli(1, p = pi_hat)*1
-  risk <- sum(dem_diag)/length(dem_diag)
+  risk_percent <- mean(dem_diag)*100
   
-  return(abs(risk - risk_match))
+  return(abs(risk_percent - risk_match))
 }
 
 #---- Function that looks for the betas ----
 find_betas <- function(obs, dem_risk){
   beta_results <- matrix(nrow = length(dem_risk), ncol = 6)
   start = 4 #represent starting age where we have data (age 70)
+  data <- obs %>% 
+    dplyr::select("sex", 
+                  variable_names$Cij_varnames[(start + 1):nrow(variable_names)], 
+                  variable_names$Fij_varnames[(start + 1):
+                                                nrow(variable_names)]) %>% 
+    mutate("dem_case" = rep(0, nrow(obs)))
+  
   for(i in 1:nrow(beta_results)){
-    sex_i <- obs$sex
-    Cij <- obs[, variable_names$Cij_varnames[start + 1]] 
-    Fij <- obs[, variable_names$Fij_varnames[start + 1]] 
+    target_measurement <- variable_names$Cij_varnames[start + 1] %>% noquote()
+    data <- data[-which(is.na(data[, eval(target_measurement)])), ] %>% 
+      filter(dem_case == 0)
     
-    sex_i_nomiss <- sex_i[-which(is.na(Cij))]
-    Cij_nomiss <- Cij[-which(is.na(Cij))]
-    Fij_nomiss <- Fij[-which(is.na(Cij))]
-    
-    if(i == 1){
-      many_opts <- replicate(100, 
+    sex_i <- data$sex
+    Cij <- data[, variable_names$Cij_varnames[start + 1]] 
+    Fij <- data[, variable_names$Fij_varnames[start + 1]] 
+
+    # if(i == 1)
+    {
+      many_opts <- replicate(10, 
                              {optim(par = rep(-0.05, 5), 
                                     fn = dem_diag, 
-                                    sex_i = sex_i_nomiss, Cij = Cij_nomiss, 
-                                    Fij = Fij_nomiss, 
+                                    sex_i = sex_i, Cij = Cij, Fij = Fij, 
                                     risk_match = dem_risk[[i]], 
                                     upper = rep(0, 5), 
                                     lower = rep(-1, 5))})
-    } else {
-      many_opts <- replicate(100, 
-                             {optim(par = beta_results[(i - 1), ], 
-                                    fn = dem_diag, 
-                                    sex_i = sex_i_nomiss, Cij = Cij_nomiss, 
-                                    Fij = Fij_nomiss, 
-                                    risk_match = dem_risk[[i]], 
-                                    upper = rep(0, 5), 
-                                    lower = rep(-1, 5))})
+    # } else {
+    #   many_opts <- replicate(10, 
+    #                          {optim(par = rep(-4.5, 5), 
+    #                                 fn = dem_diag, 
+    #                                 sex_i = sex_i_nomiss, Cij = Cij_nomiss, 
+    #                                 Fij = Fij_nomiss, 
+    #                                 risk_match = dem_risk[[i]], 
+    #                                 upper = rep(-4, 5), 
+    #                                 lower = rep(-5, 5))})
     }
-    
-    
     mean_diff <- many_opts["value", ] %>% unlist() %>% mean()
     mean_betas <- matrix(unlist(many_opts["par", ]), ncol = 5, byrow = TRUE) %>%
       colMeans()
     
     beta_results[i, ] <- c(mean_betas, mean_diff)
+    
+    mean_model <- mean_betas[1] + mean_betas[2]*Cij + mean_betas[3]*Fij + 
+      mean_betas[4]*sex_i + mean_betas[5]*Cij*Fij
+    
+    pi_hats <- exp(mean_model)/(1 + exp(mean_model))
+    data %<>% mutate("dem_case" = rbernoulli(1, p = pi_hats)*1)
+    
     start = start + 1
   }
   return(beta_results)
