bb3c0a09307b18d5be3a7a5e7a6073d0a9d48be6
Crystal Shaw <c.shaw@ucla.edu>
2018-11-17 12:28:59 -0800

    Initial commit of a file that I'm starting fresh with for searching for slopes and age-specific dementia cut-off points

---
+++ RScripts/slopes_dem-cut_search.R
@@ -0,0 +1,11 @@
+#*******************************************************************************
+# This is the function that performs the search for appropriate slope values in 
+# the model for Cij as well as age-specific dementia cut-off points
+#
+# This function is attempting to find slopes that will create dementia incidence
+# rates that match the Supplemental Table 2 in Inequalities in dementia 
+# incidence between six racial and ethnic groups over 14 years 
+# (Mayeda et al, 2016)
+#
+# We're starting with this US data, but may move to a different country's data
+#*******************************************************************************
