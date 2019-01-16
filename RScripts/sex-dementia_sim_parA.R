b99f4138086b3a1c25e8dabf30f94bb408bc89f8
Crystal Shaw <c.shaw@ucla.edu>
2019-01-16 08:53:45 -0800

    testing slopes

--- RScripts/sex-dementia_sim_parA.R
+++ RScripts/sex-dementia_sim_parA.R
@@ -60,7 +60,8 @@ b03 <- 0      #Effect of U (unmeasured/underlying variable) on cognitive interce
 #ie Cognitive slope for females age 50-70, change in cognitive slope for females age 70-85, etc...
 cij_knots <- seq(55, 95, by = 5) #Specify which ages to place knots
 #cij_slopes <- head(c(0, best_slopes_cuts$slope), -1)
-
+#test slopes
+cij_slopes <- c(0, 0, 0, 0, -0.15, 0, 0, -0.25, 0, 0)
 b11 <- 0      #Effect of sex on cognitive slope
 b12 <- -0.005 #Effect of age on cognitive slope; Note: Everyone is the same age so there is no age effect
 b13 <- -0.05  #Effect of U on cognitive slope
@@ -77,9 +78,15 @@ g6 <- 0       #Effect of cognitive function at time j on log hazard of death
 #---- Baseline hazard of death for unexposed ----
 #Computed in lambda_search_euro.R script
 #Based on 35x3000 = 105000 observations
-lambda <- c(0.00414, 0.00577, 0.00824, 0.01260, 0.02105, 0.03605, 0.06316, 
-            0.10918, 0.20142, 0.33345)
+#lambda <- c(0.00414, 0.00577, 0.00824, 0.01260, 0.02105, 0.03605, 0.06316, 
+            #0.10918, 0.20142, 0.33345)
+
+#Test US hazards
+lambda <- c(0.0211, 0.0227, 0.0243, 0.0262, 0.0280, 0.0299, 0.0327, 0.0359, 
+            0.0387, 0.0420)
 
 #---- Dementia Cut Point ----
 #Based on slopes_dem-cut_search.R script
 #dem_cuts <- head(c(-2.5, best_slopes_cuts$dem_cut), -1)
+#test dem_cuts
+dem_cuts <- rep(-3, 10)
