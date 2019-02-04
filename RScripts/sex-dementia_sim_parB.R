e9f8c5fa6d8a773a48a6abd0baff042d72243198
Crystal Shaw <c.shaw@ucla.edu>
2019-02-04 11:51:36 -0800

    Updated with new hazard values Updated with values of cij_var1 and slopes from parA file

--- RScripts/sex-dementia_sim_parB.R
+++ RScripts/sex-dementia_sim_parB.R
@@ -23,7 +23,7 @@ age0 <- rep(50, num_obs)
 int_time <- 5
 
 #Number of assessments
-num_tests <- 10
+num_tests <- 9
 
 #Resulting visit times
 visit_times <- seq(from = 0, to = int_time*num_tests, by = int_time)
@@ -34,25 +34,25 @@ psex <- 0.49
 
 #---- Variances and correlations ----
 cij_var0 <- 0.2   #Variance of random cognitive intercept
-cij_var1 <- c(seq(0.001, 0.0064, len = 7), 0.009, 0.0149, rep(0.025, 2)) #Time-dependent variance of random cognitive slope
+cij_var1 <- c(seq(0.001, 0.00233, len = 5), 0.0035, 0.005, 0.011, 0.015, 0.3) #Time-dependent variance of random cognitive slope
 cij_cov <- 0.01   #Covariance of random intercept and random slope
 cij_var3 <- 1     #Variance of noise for Cij (cognitive function for person i at time j)
 cij_r1 <- 0.3     #Correlation between noise terms for Cij; this may need to be adjusted
 #cij_var4 <- 0.19     #Variance of measurement error of Cij
 
 #---- Parameters for Cij ----
-#Read in the parameter table from the slopes_dem-cut_search.R script
-search_results <-
-  read_csv(paste0(here("Data", 
-                  paste0("best_slopes_cuts_20190124.csv"))))
-
-#Filling in the intermediate results table (just to check)
-if(nrow(search_results != 10)){
-  row <- nrow(search_results)
-  search_results[(row + 1):10, "slope"] <- 0
-  search_results[(row + 1):10, "dem_cut"] <- search_results[row, "dem_cut"]
-  search_results[, "age"] <- seq(55, 100, by = 5)
-}
+# #Read in the parameter table from the slopes_dem-cut_search.R script
+# search_results <-
+#   read_csv(paste0(here("Data", 
+#                   paste0("best_slopes_cuts_20190124.csv"))))
+# 
+# #Filling in the intermediate results table (just to check)
+# if(nrow(search_results != 10)){
+#   row <- nrow(search_results)
+#   search_results[(row + 1):10, "slope"] <- 0
+#   search_results[(row + 1):10, "dem_cut"] <- search_results[row, "dem_cut"]
+#   search_results[, "age"] <- seq(55, 100, by = 5)
+# }
 
 #Cognitive function for person i at time j
 b00 <- 0      #Cognitive intercept for females
@@ -63,11 +63,11 @@ b03 <- 0      #Effect of U (unmeasured/underlying variable) on cognitive interce
 #First value is cognitive slope, the remaining values are changes in cognitive slopes
 #These are: b10a, b10b - b10a, b10c - b10b, etc...
 #ie Cognitive slope for females age 50-70, change in cognitive slope for females age 70-85, etc...
-cij_knots <- seq(55, 95, by = 5) #Specify which ages to place knots
-#test slopes
-#cij_slopes <- c(0, 0, 0, 0, -0.15, 0, 0, -0.25, 0, 0)
+cij_knots <- seq(55, 90, by = 5) #Specify which ages to place knots
 
-cij_slopes <- head(c(0, search_results$slope), -1)
+#From parA for now
+cij_slopes <- c(-0.00607, -0.0184, -0.0277, -0.0379, -0.00000108, -0.0274, 
+                -0.0326, -0.202, -0.4)
 
 b11 <- 0      #Effect of sex on cognitive slope
 b12 <- -0.005 #Effect of age on cognitive slope; Note: Everyone is the same age so there is no age effect
@@ -85,18 +85,11 @@ g6 <- 0       #Effect of cognitive function at time j on log hazard of death
 #---- Baseline hazard of death for unexposed ----
 #Computed in lambda_search_euro.R script
 #Based on 35x3000 = 105000 observations
-#lambda <- c(0.00414, 0.00577, 0.00824, 0.01260, 0.02105, 0.03605, 0.06316, 
-            #0.10918, 0.20142, 0.33345)
-
-#Test US hazards
-lambda <- c(0.0211, 0.0227, 0.0243, 0.0262, 0.0280, 0.0299, 0.0327, 0.0359, 
-            0.0387, 0.0420)
+lambda <- c(0.00334, 0.00486, 0.00719, 0.01116, 0.01933, 0.03444, 0.06568, 
+            0.13646, 0.32339) 
 
 #---- Dementia Cut Point ----
-#Based on slopes_dem-cut_search.R script
-
-#dem_cuts <- head(c(-2.5, best_slopes_cuts$dem_cut), -1)
-#test dem_cuts
-#dem_cuts <- rep(-3, 10)
-dem_cuts <- head(c(search_results$dem_cut[1], search_results$dem_cut), -1)
+#From parA
+dem_cuts <- c(-4, -3.51, -3.56, -3.82, -4.70, -5.29, -5.55, -6.12, -6.6, 
+              -6.65)
 
