1fb305afbd572e135166d0560b99c4c106d0cf9b
Crystal Shaw <c.shaw@ucla.edu>
2019-02-25 19:28:27 -0800

    Keep all the data separate for now so increased the number of inputs

--- RScripts/survival_times.R
+++ RScripts/survival_times.R
@@ -1,19 +1,19 @@
 #---- Model for Survival Time ----
-survival <- function(obs_matrix, lambda){
+survival <- function(obs_matrix, lambda, rij, agec, cij_slope, Cij){
   #Calculate survival times for each interval
   Sij <- matrix(nrow = nrow(obs_matrix), ncol = (length(visit_times) - 1))
   for(i in 1:nrow(obs_matrix)){
     survtimes <- matrix(NA, ncol = (length(visit_times) - 1), nrow = 1)
     for(j in 1:length(survtimes)){
-      r <- variable_names$rij_varnames[j]
-      agec <- variable_names$agec_varnames[j]
-      cij_slope <- variable_names$cij_slopeij_varnames[j]
-      C <- variable_names$Cij_varnames[j]
-      survtime = -log(obs_matrix[i, r])/
-        (lambda[j]*exp(g1[j]*obs_matrix[i, "sex"] + g2*obs_matrix[i, agec] + 
+      r_name <- variable_names$rij_varnames[j]
+      agec_name <- variable_names$agec_varnames[j]
+      cij_slope_name <- variable_names$cij_slopeij_varnames[j]
+      C_name <- variable_names$Cij_varnames[j]
+      survtime = -log(rij[i, r_name])/
+        (lambda[j]*exp(g1[j]*obs_matrix[i, "sex"] + g2*agec[i, agec_name] + 
                          g3*obs_matrix[i, "U"] + 
-                         g4*obs_matrix[i, "sex"]*obs_matrix[i, agec] + 
-                         g5*obs_matrix[i, cij_slope] + g6*obs_matrix[i, C]))
+                         g4*obs_matrix[i, "sex"]*agec[i, agec_name] + 
+                         g5*cij_slope[i, cij_slope_name] + g6*Cij[i, C_name]))
       survtimes[j] <- as.numeric(survtime)
       if(survtime < 5){
         break
