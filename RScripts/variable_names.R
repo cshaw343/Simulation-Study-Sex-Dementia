2b40629b86050aaecc5e80a8fe4e2e95c4adaba0
Crystal Shaw <c.shaw@ucla.edu>
2019-02-28 17:24:37 -0800

    Added contributed_varnames to column names

--- RScripts/variable_names.R
+++ RScripts/variable_names.R
@@ -91,5 +91,5 @@ column_names <- c(na.omit(variable_names$exo_var), variable_names$age_varnames,
                   "study_death", "survtime", "age_death", 
                   variable_names$dem_varnames, "dem_wave", "dem", "timetodem", 
                   "ageatdem", "dem_death", "timetodem_death", "ageatdem_death", 
-                  "dem_alive")
+                  "dem_alive", variable_names$contributed_varnames)
 
