a1d8239dfbddda08175355b1d5f55f06ef54f38a
Crystal Shaw <c.shaw@ucla.edu>
2019-03-01 12:33:32 -0800

    Removed commented code because results with refactored code are correct

--- RScripts/cognitive_function_model.R
+++ RScripts/cognitive_function_model.R
@@ -41,38 +41,3 @@ cog_func <- function(knots_ages, slopes, obs_matrix){
   
   return(list("Cij" = Cij, "slopes" = slopes))
 }
-
-  
-  
-  
-  
-  
-  # for(i in 1:nrow(Cij)){
-  #   cij_values <- matrix(NA, ncol = (length(visit_times)), nrow = 1)
-  #   for(j in 1:length(visit_times)){
-  #     t = visit_times[j]
-  #     test_num = test_nums[j]
-  #     eps_name <- paste("eps", test_num, sep = "")
-  #     z0_name <- paste0("z0_", (j - 1), "i")
-  #     z1_name <- paste0("z1_", (j - 1), "i")
-  #     if(ages[j] <= knots_ages[1]){
-  #       cij_values[j] = b00 + obs_matrix[i, z0_name] + 
-  #         b01*obs_matrix[i, "sex"] + b02*obs_matrix[i, "age0_c50"] + 
-  #         b03*obs_matrix[i, "U"] + obs_matrix[i, eps_name] +
-  #         (extend_slopes[1] + obs_matrix[i, z1_name] + 
-  #            b11*obs_matrix[i, "sex"] + b12*obs_matrix[i, "age0_c50"] + 
-  #            b13*obs_matrix[i, "U"])*t
-  #     } else{
-  #       cij_values[j] = b00 + obs_matrix[i, z0_name] + 
-  #         b01*obs_matrix[i, "sex"] + b02*obs_matrix[i, "age0_c50"] + 
-  #         b03*obs_matrix[i, "U"] + obs_matrix[i, eps_name] +
-  #         sum(testXslope[1:(test_num - 1)]) +
-  #         (sum(extend_slopes[1:test_num]) + obs_matrix[i, z1_name] + 
-  #            b11*obs_matrix[i, "sex"] +
-  #            b12*obs_matrix[i, "age0_c50"] + b13*obs_matrix[i, "U"])*t
-  #     }
-  #   }
-  #   Cij[i, ] <- cij_values
-  # }
-  # 
-  # 
