6a57ad5e16caadf13c6bc640056adaffc9f68567
Crystal Shaw <c.shaw@ucla.edu>
2019-03-01 16:39:23 -0800

    Prep for running on desktop

--- RScripts/sex-dementia_sim_run.R
+++ RScripts/sex-dementia_sim_run.R
@@ -2,28 +2,31 @@
 if (!require("pacman")) 
   install.packages("pacman", repos='http://cran.us.r-project.org')
 
-p_load("future.apply")
+p_load("future.apply", "parallel")
 
 set.seed(20190213)
 
 #---- Source Files ----
-source("RScripts/sex-dementia_sim_parA.R")    #The parameter file
-source("RScripts/variable_names.R")           #Creates all the variable names
-source("RScripts/sex-dementia_sim_script.R")  #The simulation script
-source("RScripts/misc_custom_functions.R")    #Other functions needed
+source("RScripts/sex-dementia_sim_parA.R")          #The parameter file
+source("RScripts/sex-dementia_sim_data_gen.R")      #The data generation script
+source("RScripts/sex-dementia_sim_data_analysis.R") #The data analysis script
+source("RScripts/misc_custom_functions.R")          #Other functions needed
+
+#---- Generating one cohort ----
+data_gen() %>% saveRDS("Data/test_sim_results_A_20190301")
 
 #---- Running the simulation in parallel----
-runs = 1000
-plan(multiprocess, workers = (detectCores() - 5))
-sim_results <- future_replicate(runs, sex_dem_sim())
+runs = 100
+plan(multiprocess, workers = (detectCores() - 2))
+sim_IRRs <- future_replicate(runs, sex_dem_sim())
 
-#---- Converting results to usable format ----
-results_mat <- matrix(unlist(sim_results), 
-                         nrow = runs, ncol = ncol(sim_results), byrow = TRUE)
-  
-results_mat %<>% as.data.frame() %>% 
-  set_colnames(dimnames(sim_results)[[2]]) %>%
-  saveRDS("Data/logIRRs_A_20190222")
+#Saving results
+results_mat <- matrix(unlist(sim_IRRs), 
+                      nrow = runs, ncol = ncol(sim_IRRs), byrow = TRUE)
+
+results_mat %>% as.data.frame() %>% 
+  set_colnames(dimnames(sim_IRRs)[[2]]) %>%
+  saveRDS("Data/logIRRs_A_20190301")
 
 
 
