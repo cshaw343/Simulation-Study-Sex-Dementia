1f4d1faf5e8bef8f0cc57387d042dffcf76ddcff
Crystal Shaw <c.shaw@ucla.edu>
2019-03-01 16:44:21 -0800

    vectorized version of dem_onset

--- RScripts/dementia_onset.R
+++ RScripts/dementia_onset.R
@@ -1,23 +1,20 @@
 #---- Function for computing dementia onset ----
-dem_onset <- function(obs, dem_cuts){
+dem_onset <- function(obs_matrix, dem_cuts){
   #Function we are trying to find the root of
   event_est <- function(t, M, B, dem_cut){
     cij = B + M*t
     return(abs(cij - dem_cut))
   }
-  timetodem <- vector(length = nrow(obs))
-  for(i in 1:nrow(obs)){
-    data <- obs[i, ]
-    if(is.na(data["dem_wave"])){
-      timetodem[i] = as.double(data["survtime"])
-    } else if(data["dem_wave"] == 0){
-      timetodem[i] = 0
+  timetodem <- vector(length = nrow(obs_matrix))
+  for(i in 1:nrow(obs_matrix)){
+    if(is.na(obs_matrix[i, "dem_wave"])){
+      timetodem[i] = as.double(obs_matrix[i, "survtime"])
     } else {
-      wave = as.double(data["dem_wave"])
+      wave = as.double(obs_matrix[i, "dem_wave"])
       slope <- paste("cij_slope", wave - 1, "-", wave, sep = "")
       int <- paste("Ci", wave - 1, sep = "")
-      m = as.double(data[slope])
-      b = as.double(data[int])
+      m = as.double(obs_matrix[i, slope])
+      b = as.double(obs_matrix[i, int])
       timetodem[i] = (wave - 1)*int_time + 
         optimize(event_est, M = m, 
                  B = b, dem_cut = dem_cuts[(wave + 1)], 
