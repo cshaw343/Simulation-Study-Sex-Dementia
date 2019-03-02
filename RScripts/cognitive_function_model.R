4599dd0b1f2842a8a323bbec1b5c9e90013d7d96
Crystal Shaw <c.shaw@ucla.edu>
2019-03-01 16:43:58 -0800

    Vectorized version of cog_func

--- RScripts/cognitive_function_model.R
+++ RScripts/cognitive_function_model.R
@@ -1,5 +1,5 @@
 #---- Model for Cognitive Function ----
-cog_func <- function(knots_ages, slopes, obs){
+cog_func <- function(knots_ages, slopes, obs_matrix){
   extend_slopes <- c(slopes[1], rep(0, num_tests - 1))
   ages <- visit_times + 50
   for(k in 1:length(knots_ages)){
@@ -8,33 +8,36 @@ cog_func <- function(knots_ages, slopes, obs){
   mid_visits <- visit_times[c(-1, -length(visit_times))]
   test_nums = seq(from = 0, to = num_tests, by = 1)
   testXslope = (-1)*extend_slopes[-1]*mid_visits
-  Cij <- vector(length = length(visit_times))
-  for(j in 1:length(visit_times)){
+  Cij <- matrix(nrow = nrow(obs_matrix), ncol = length(visit_times))
+  for(j in 1:ncol(Cij)){
     t = visit_times[j]
     test_num = test_nums[j]
-    eps <- paste("eps", test_num, sep = "")
-    z0 <- paste0("z0_", (j - 1), "i")
-    z1 <- paste0("z1_", (j - 1), "i")
+    eps_name <- paste("eps", test_num, sep = "")
+    z0_name <- paste0("z0_", (j - 1), "i")
+    z1_name <- paste0("z1_", (j - 1), "i")
     if(ages[j] <= knots_ages[1]){
-      Cij[j] = b00 + obs[, z0] + b01*obs[, "sex"] + 
-        b02*obs[, "age0_c50"] + b03*obs[, "U"] + obs[, eps] +
-        (extend_slopes[1] + obs[, z1] + b11*obs[, "sex"] +
-           b12*obs[, "age0_c50"] + b13*obs[, "U"])*t
+      Cij[, j] = b00 + obs_matrix[, z0_name] + 
+        b01*obs_matrix[, "sex"] + b02*obs_matrix[, "age0_c50"] + 
+        b03*obs_matrix[, "U"] + obs_matrix[, eps_name] +
+        (extend_slopes[1] + obs_matrix[, z1_name] + 
+           b11*obs_matrix[, "sex"] + b12*obs_matrix[, "age0_c50"] + 
+           b13*obs_matrix[, "U"])*t
     } else{
-      Cij[j] = b00 + obs[, z0] + b01*obs[, "sex"] +
-        b02*obs[, "age0_c50"] + b03*obs[, "U"] + obs[, eps] +
+      Cij[, j] = b00 + obs_matrix[, z0_name] + 
+        b01*obs_matrix[, "sex"] + b02*obs_matrix[, "age0_c50"] + 
+        b03*obs_matrix[, "U"] + obs_matrix[, eps_name] +
         sum(testXslope[1:(test_num - 1)]) +
-        (sum(extend_slopes[1:test_num]) + obs[, z1] + b11*obs[, "sex"] +
-           b12*obs[, "age0_c50"] + b13*obs[, "U"])*t
+        (sum(extend_slopes[1:test_num]) + obs_matrix[, z1_name] + 
+           b11*obs_matrix[, "sex"] +
+           b12*obs_matrix[, "age0_c50"] + b13*obs_matrix[, "U"])*t
     }
   }
-  slopes <- matrix(NA, nrow = nrow(obs), ncol= (length(visit_times) - 1))
-  #Cij is stored as a list in this function environment so use list indexing
-  for(j in 1:ncol(slopes)){
-    b <- Cij[[j + 1]]
-    a <- Cij[[j]]
-    slopes[, j] = (b-a)/int_time
+  
+  slopes <- matrix(NA, nrow = nrow(obs_matrix), ncol= (length(visit_times) - 1))
+  
+  for(i in 1:ncol(slopes)){
+    slopes[, i] <- (Cij[, i + 1] - Cij[, i])/int_time
   }
+  
   return(list("Cij" = Cij, "slopes" = slopes))
 }
-
