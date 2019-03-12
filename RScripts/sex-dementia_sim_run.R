e37b09d6e56e637137985f29e4505839d236922e
Crystal Shaw <c.shaw@ucla.edu>
2019-03-11 23:31:13 -0700

    Added all the summary of means for simulation results

--- RScripts/sex-dementia_sim_run.R
+++ RScripts/sex-dementia_sim_run.R
@@ -4,7 +4,7 @@ if (!require("pacman"))
 
 p_load("future.apply", "parallel", "here")
 
-set.seed(20190213)
+set.seed(20190311)
 
 #---- Source Files ----
 source(here("RScripts", "sex-dementia_sim_parA.R"))          #The parameter file
@@ -13,21 +13,35 @@ source(here("RScripts", "sex-dementia_sim_data_analysis.R")) #The data analysis 
 source(here("RScripts", "misc_custom_functions.R"))          #Other functions needed
 
 #---- Generating one cohort ----
-data_gen() %>% saveRDS(here("Data", "test_sim_results_A_20190310"))
+data_gen() %>% saveRDS(here("Data", "test_sim_results_A_20190311"))
 
 #---- Running the simulation in parallel----
 start_time <- Sys.time()
-runs = 100
-plan(multiprocess, workers = (detectCores() - 4))
-sim_IRRs <- future_replicate(runs, sex_dem_sim())
+runs = 1000
+plan(multiprocess, workers = (detectCores() - 2))
+sim_results <- future_replicate(runs, sex_dem_sim())
+Sys.time() - start_time
 
-#Saving results
-results_mat <- matrix(unlist(sim_IRRs), 
-                      nrow = runs, ncol = ncol(sim_IRRs), byrow = TRUE)
+#Means of results
+mean_num_obs <- mean(unlist(sim_results["num_obs", ]))
+mean_num_females <- mean(unlist(sim_results["num_females", ]))
+mean_num_males <- mean(unlist(sim_results["num_males", ]))
 
-results_mat %>% as.data.frame() %>% 
-  set_colnames(dimnames(sim_IRRs)[[2]]) %>%
-  saveRDS(here("Data", "logIRRs_A_20190304"))
+mean_at_risk_by_sex <- sim_results["at_risk_by_sex", ] %>% 
+  Reduce(`+`, .)/runs
+mean_inc_cases_by_sex <- sim_results["inc_cases_by_sex", ] %>% 
+  Reduce(`+`, .)/runs
+mean_dem_cases_by_sex <- sim_results["dem_cases_by_sex", ] %>% 
+  Reduce(`+`, .)/runs
+mean_PY_by_sex <- sim_results["PY_by_sex", ] %>% 
+  Reduce(`+`, .)/runs
+mean_dem_prob_by_sex <- sim_results["dem_prob_by_sex", ] %>% 
+  Reduce(`+`, .)/runs
+
+mean_sim_rates <- sim_results["sim_rates", ] %>% Reduce(`+`, .)/runs
+
+#Female:Male
+mean_IRRs <- exp(sim_results["logIRRs", ] %>% Reduce(`+`, .)/runs) 
 
 Sys.time() - start_time
 
