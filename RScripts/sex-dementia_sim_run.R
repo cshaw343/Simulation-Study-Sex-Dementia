87e1e9adf8e63a9e47d51596fed8b4432fa88f58
Crystal Shaw <c.shaw@ucla.edu>
2019-03-12 16:04:46 -0700

    Added all the means for the additional analyses

--- RScripts/sex-dementia_sim_run.R
+++ RScripts/sex-dementia_sim_run.R
@@ -2,7 +2,7 @@
 if (!require("pacman")) 
   install.packages("pacman", repos='http://cran.us.r-project.org')
 
-p_load("future.apply", "parallel", "here")
+p_load("future.apply", "parallel", "here", "magrittr")
 
 set.seed(20190311)
 
@@ -13,37 +13,87 @@ source(here("RScripts", "sex-dementia_sim_data_analysis.R")) #The data analysis 
 source(here("RScripts", "misc_custom_functions.R"))          #Other functions needed
 
 #---- Generating one cohort ----
-data_gen() %>% saveRDS(here("Data", "test_sim_results_A_20190311"))
+data_gen() %>% saveRDS(here("Data", "test_sim_results_A_20190312"))
 
 #---- Running the simulation in parallel----
 start_time <- Sys.time()
-runs = 1000
 plan(multiprocess, workers = (detectCores() - 2))
 sim_results <- future_replicate(runs, sex_dem_sim())
+
+#Mean results
+mean_results_mat <- data.frame(matrix(NA, nrow = num_tests, 
+                                      ncol = nrow(sim_results))) %>%
+  set_colnames(c("num_obs", "num_females", "num_males", "p_alive", 
+                 "p_alive_females", "p_alive_males", "mortality_HRs(F:M)",
+                 "at_risk_females", "at_risk_males", "inc_cases_females", 
+                 "inc_cases_males", "dem_cases_females", "dem_cases_males", 
+                 "PY_females", "PY_males", "dem_prob_females", "dem_prob_males", 
+                 "sim_rates","sim_rates_females", "sim_rates_males", 
+                 "IRRs(F:M)", "dem_HRs(F:M)"))
+
+mean_results_mat[1, "num_obs"] <- mean(unlist(sim_results["num_obs", ]))
+mean_results_mat[1, "num_females"] <- mean(unlist(sim_results["num_females", ]))
+mean_results_mat[1, "num_males"] <- mean(unlist(sim_results["num_males", ]))
+
+mean_results_mat[, "p_alive"] <- sim_results["p_alive", ] %>% 
+  Reduce(`+`, .)/runs
+mean_results_mat[, "p_alive_females"] <- sim_results["p_alive_females", ] %>% 
+  Reduce(`+`, .)/runs
+mean_results_mat[, "p_alive_males"] <- sim_results["p_alive_males", ] %>% 
+  Reduce(`+`, .)/runs
+mean_results_mat[, "mortality_HRs(F:M)"] <- 
+  exp(sim_results["simulated_mortality_logHRs", ] %>% Reduce(`+`, .)/runs)
+
+mean_results_mat[, "at_risk_females"] <- 
+  sim_results["at_risk_females", ] %>% Reduce(`+`, .)/runs
+mean_results_mat[, "at_risk_males"] <- 
+  sim_results["at_risk_males", ] %>% 
+  Reduce(`+`, .)/runs
+
+mean_results_mat[, "inc_cases_females"] <- 
+  sim_results["inc_cases_females", ] %>% Reduce(`+`, .)/runs
+mean_results_mat[, "inc_cases_males"] <- 
+  sim_results["inc_cases_males", ] %>% 
+  Reduce(`+`, .)/runs
+
+mean_results_mat[, "dem_cases_females"] <- 
+  sim_results["dem_cases_female", ] %>% Reduce(`+`, .)/runs
+mean_results_mat[, "dem_cases_males"] <- 
+  sim_results["dem_cases_male", ] %>% 
+  Reduce(`+`, .)/runs
+
+mean_results_mat[, "PY_females"] <- 
+  sim_results["PY_females", ] %>% Reduce(`+`, .)/runs
+mean_results_mat[, "PY_males"] <- 
+  sim_results["PY_males", ] %>% Reduce(`+`, .)/runs
+
+mean_results_mat[, "dem_prob_females"] <- 
+  sim_results["dem_prob_females", ] %>% Reduce(`+`, .)/runs
+mean_results_mat[, "dem_prob_males"] <- 
+  sim_results["dem_prob_males", ] %>% 
+  Reduce(`+`, .)/runs
+
+mean_results_mat[, "sim_rates"] <- 
+  sim_results["sim_rates", ] %>% Reduce(`+`, .)/runs
+mean_results_mat[, "sim_rates_females"] <- 
+  sim_results["sim_rates_females", ] %>% Reduce(`+`, .)/runs
+mean_results_mat[, "sim_rates_males"] <- 
+  sim_results["sim_rates_males", ] %>% Reduce(`+`, .)/runs
+
+mean_results_mat[, "IRRs(F:M)"] <- 
+  exp(sim_results["logIRRs", ] %>% Reduce(`+`, .)/runs) 
+
+mean_results_mat[, "dem_HRs(F:M)"] <- 
+  exp(sim_results["simulated_dementia_logHRs", ] %>% Reduce(`+`, .)/runs)
+
+write_csv(mean_results_mat,
+            here("Results", "Scenario_A_no_bias", 
+                 paste0("sim_results_", gsub("-", "", Sys.Date()), ".csv")))
+
 Sys.time() - start_time
 
-#Means of results
-mean_num_obs <- mean(unlist(sim_results["num_obs", ]))
-mean_num_females <- mean(unlist(sim_results["num_females", ]))
-mean_num_males <- mean(unlist(sim_results["num_males", ]))
 
-mean_at_risk_by_sex <- sim_results["at_risk_by_sex", ] %>% 
-  Reduce(`+`, .)/runs
-mean_inc_cases_by_sex <- sim_results["inc_cases_by_sex", ] %>% 
-  Reduce(`+`, .)/runs
-mean_dem_cases_by_sex <- sim_results["dem_cases_by_sex", ] %>% 
-  Reduce(`+`, .)/runs
-mean_PY_by_sex <- sim_results["PY_by_sex", ] %>% 
-  Reduce(`+`, .)/runs
-mean_dem_prob_by_sex <- sim_results["dem_prob_by_sex", ] %>% 
-  Reduce(`+`, .)/runs
 
-mean_sim_rates <- sim_results["sim_rates", ] %>% Reduce(`+`, .)/runs
-
-#Female:Male
-mean_IRRs <- exp(sim_results["logIRRs", ] %>% Reduce(`+`, .)/runs) 
-
-Sys.time() - start_time
 
 
 
