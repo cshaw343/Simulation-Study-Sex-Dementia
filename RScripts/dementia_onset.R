91d1e43c2ee721423078705e5429d0accd424640
Crystal Shaw <c.shaw@ucla.edu>
2019-04-19 22:31:54 -0700

    Interpolate dem_cuts to 1-year dem_cuts

--- RScripts/dementia_onset.R
+++ RScripts/dementia_onset.R
@@ -5,6 +5,7 @@ dem_onset <- function(obs_matrix, dem_cuts){
     cij = B + M*t
     return(abs(cij - dem_cut))
   }
+    
   timetodem <- vector(length = nrow(obs_matrix))
   for(i in 1:nrow(obs_matrix)){
     if(is.na(obs_matrix[i, "dem_wave"])){
@@ -15,10 +16,18 @@ dem_onset <- function(obs_matrix, dem_cuts){
       int <- paste("Ci", wave - 1, sep = "")
       m = as.double(obs_matrix[i, slope])
       b = as.double(obs_matrix[i, int])
-      timetodem[i] = (wave - 1)*int_time + 
-        optimize(event_est, M = m, 
-                 B = b, dem_cut = dem_cuts[(wave + 1)], 
-                 interval = c(0, 5))$minimum
+      interp_dem_cuts = seq(dem_cuts[wave], dem_cuts[wave + 1], length = 5)
+      
+      for(j in 1:length(interp_dem_cuts)){
+        test_time = optimize(event_est, M = m, B = b, 
+                             dem_cut = interp_dem_cuts[j], 
+                             interval = c(0, 5))$minimum
+        
+        if(test_time > (j - 1) & test_time <= j){
+          timetodem[i] = (wave - 1)*int_time + test_time
+          break
+        }
+      }
     }
   }
   return(timetodem)
