52b1b835c26acc6aabfc84c2b6e3e639adcb36e7
Crystal Shaw <c.shaw@ucla.edu>
2019-04-28 14:54:26 -0700

    Coded up random draw from a bernoulli for adjacent dementia cut offs-- didn't fix IRR issue

--- RScripts/dementia_onset.R
+++ RScripts/dementia_onset.R
@@ -18,22 +18,10 @@ dem_onset <- function(obs_matrix, dem_cuts){
       b = as.double(obs_matrix[i, int])
       interp_dem_cuts = seq(dem_cuts[wave], dem_cuts[wave + 1], length = 6)
       
-      #Linear interpolation of dementia cutoffs
-      for(j in 1:(length(interp_dem_cuts) - 1)){
-        test_time = optimize(event_est, M = m, B = b, 
-                             dem_cut = interp_dem_cuts[j + 1], 
-                             interval = c(0, 5))$minimum
-        
-        if(test_time > (j - 1) & test_time <= j){
-          timetodem[i] = (wave - 1)*int_time + test_time
-          break
-        }
-      }
-      
-      # #Probabilistic draw of dementia cutoffs
-      # for(j in 1:length(interp_dem_cuts)){
+      # #Linear interpolation of dementia cutoffs
+      # for(j in 1:(length(interp_dem_cuts) - 1)){
       #   test_time = optimize(event_est, M = m, B = b, 
-      #                        dem_cut = interp_dem_cuts[j], 
+      #                        dem_cut = interp_dem_cuts[j + 1], 
       #                        interval = c(0, 5))$minimum
       #   
       #   if(test_time > (j - 1) & test_time <= j){
@@ -41,6 +29,22 @@ dem_onset <- function(obs_matrix, dem_cuts){
       #     break
       #   }
       # }
+      
+      #Probabilistic draw of dementia cutoffs
+      for(j in 1:length(interp_dem_cuts)){
+        if(rbernoulli(1, p = 0.75)){
+          dem_cut = interp_dem_cuts[j + 1]
+        } else {
+          dem_cut = interp_dem_cuts[j]
+        }
+        test_time = optimize(event_est, M = m, B = b,
+                             dem_cut = dem_cut,
+                             interval = c(0, 5))$minimum
+        if(test_time > (j - 1) & test_time <= j){
+          timetodem[i] = (wave - 1)*int_time + test_time
+          break
+        }
+      }
     }
   }
   return(timetodem)
