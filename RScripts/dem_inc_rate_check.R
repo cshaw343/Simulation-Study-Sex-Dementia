43a6be3e3e4389ad2b4e0a7d07d2219fb1d989d0
Crystal Shaw <c.shaw@ucla.edu>
2019-05-19 12:02:17 -0700

    Added things to sanity check script

--- RScripts/dem_inc_rate_check.R
+++ RScripts/dem_inc_rate_check.R
@@ -9,17 +9,27 @@ options(digits = 6)   #Round to 6 decimal places
 options(warn = -1)    #Suppress warnings
 
 #---- Source files ----
-source(here("RScripts", "variable_names.R"))
 source(here("RScripts", "sex-dementia_sim_parA.R"))
+source(here("RScripts", "variable_names.R"))
 source(here("RScripts", "sex-dementia_sim_data_gen.R"))
+
+#---- Plug in newly optimized data ----
+cij_slopes <- c(0.0000000, -0.0576876, -0.1313314, -0.2601800, -0.3294733,  
+                0.0000000,  0.0000000,  0.0000000,  0.0000000, 0.0000000)
+
+cij_var1 <- c(0.00100000, 0.00100000, 0.00199805, 0.00200000, 0.00200178, 
+              0.00100000, 0.00100000, 0.00100000, 0.00100000, 0.00100000)
+
+lambda <- c(0.00414, 0.00577, 0.00824, 0.01260, 0.02105, 0.03605, 0.06316, 
+            0.10918, 0.20142)
 
 #---- Generate the data ----
 data <- data_gen()
 
 #---- Compute incidence rates ----
-sim_rates <- matrix(ncol = 9, nrow = 1)
-colnames(sim_rates) <- na.omit(variable_names$interval_ages)
-rownames(sim_rates) <- c("")
+sim_inc_rates <- matrix(ncol = 9, nrow = 1)
+colnames(sim_inc_rates) <- na.omit(variable_names$interval_ages)
+rownames(sim_inc_rates) <- c("")
 
 for(slot in 1:num_tests){
   if(slot == 1){
@@ -41,7 +51,31 @@ for(slot in 1:num_tests){
     filter(!! as.name(death_last_wave) == 0 & 
              !! as.name(dem_last_wave) == 0) 
   
-  sim_rates[1, slot] = round(1000*(sum(PY_data[, dem_this_wave], 
+  sim_inc_rates[1, slot] = round(1000*(sum(PY_data[, dem_this_wave], 
                                        na.rm = TRUE)/
                                      sum(PY_data[, contributed])), 3)
 }
+
+#---- Compute survival data ----
+#---- Data by sex ----
+female_data <- data %>% filter(sex == 0)
+male_data <- data %>% filter(sex == 1)
+
+#---- Cohort size ----
+num_obs <- nrow(data)
+num_females <- nrow(female_data)
+num_males <- nrow(male_data)
+
+#---- Survival probabilities ----
+p_alive <- data %>% 
+  dplyr::select(na.omit(variable_names$deathij_varnames)) %>% 
+  map_dbl(~sum(. == 0, na.rm = TRUE))/num_obs
+
+#---- Survival probabilities by sex ----
+p_alive_females <- female_data %>%
+  dplyr::select(na.omit(variable_names$deathij_varnames)) %>% 
+  map_dbl(~sum(. == 0, na.rm = TRUE))/num_females
+
+p_alive_males <- male_data %>%
+  dplyr::select(na.omit(variable_names$deathij_varnames)) %>% 
+  map_dbl(~sum(. == 0, na.rm = TRUE))/num_males
