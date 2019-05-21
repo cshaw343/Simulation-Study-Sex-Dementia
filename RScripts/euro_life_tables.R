0eab891e5f30411bd8945e8fe1cc52dd3c19fec0
Crystal Shaw <c.shaw@ucla.edu>
2019-05-21 10:18:58 -0700

    Comment out all the plotting

--- RScripts/euro_life_tables.R
+++ RScripts/euro_life_tables.R
@@ -270,15 +270,15 @@ HR_plot_data <-
   mutate("Age" = seq(0, 100, by = 5)) %>%
   gather(contains("HR"), key = "Country", value = "HR")
   
-#Plot Hazard Ratios 
-all_countries_1920_1925_HR <- ggplot(HR_plot_data, aes(Age, HR)) + 
-  geom_line(aes(color = Country, group = Country), size = 1.25, alpha = 0.6) +
-  labs(y = "Hazard Ratio (Male:Female)", x = "Age", 
-       color = "") + theme_minimal() + 
-  ggtitle("1920-1925 Birth Cohort")
-
-ggsave(filename = "Plots/1920-1925_birth_cohort_HR.jpeg", width = 10, 
-       height = 7, plot = all_countries_1920_1925_HR)
+# #Plot Hazard Ratios 
+# all_countries_1920_1925_HR <- ggplot(HR_plot_data, aes(Age, HR)) + 
+#   geom_line(aes(color = Country, group = Country), size = 1.25, alpha = 0.6) +
+#   labs(y = "Hazard Ratio (Male:Female)", x = "Age", 
+#        color = "") + theme_minimal() + 
+#   ggtitle("1920-1925 Birth Cohort")
+# 
+# ggsave(filename = "Plots/1920-1925_birth_cohort_HR.jpeg", width = 10, 
+#        height = 7, plot = all_countries_1920_1925_HR)
 
 
 
