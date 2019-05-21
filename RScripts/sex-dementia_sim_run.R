e482c5a613a5e827d3a4228461a49ce7f70558b9
Crystal Shaw <c.shaw@ucla.edu>
2019-05-20 17:51:58 -0700

    Change file name

--- RScripts/sex-dementia_sim_run.R
+++ RScripts/sex-dementia_sim_run.R
@@ -54,7 +54,7 @@ for(i in output_column_names){
 }
 
 write_csv(results_matrix, here("Results", "Scenario_A_no_bias", 
-                               "sim_results_20_20190415.csv"))
+                               "unform_timetodem_100_20190505.csv"))
   
 
 
