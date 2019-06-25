7c392514a48f8532f92c4b9032fa3aff59343a18
Crystal Shaw <c.shaw@ucla.edu>
2019-06-25 16:05:48 -0700

    Changed survival time calculation to go column-wise rather than row-wise

--- RScripts/survival_times.R
+++ RScripts/survival_times.R
@@ -1,22 +1,22 @@
 #---- Model for Survival Time ----
 survival <- function(obs_matrix){
   #Calculate survival times for each interval
-  Sij <- matrix(nrow = nrow(obs_matrix), ncol = (length(visit_times) - 1))
-  for(i in 1:nrow(obs_matrix)){
-    survtimes <- matrix(NA, ncol = (length(visit_times) - 1), nrow = 1)
+  Sij <- matrix(ncol = ncol(obs_matrix), nrow = (length(visit_times) - 1))
+  for(i in 1:ncol(obs_matrix)){
+    survtimes <- matrix(NA, nrow = (length(visit_times) - 1), ncol = 1)
     for(j in 1:length(survtimes)){
       r_name <- variable_names$rij_varnames[j]
       agec_name <- variable_names$agec_varnames[j]
       cij_slope_name <- variable_names$cij_slopeij_varnames[j]
       C_name <- variable_names$Cij_varnames[j]
       dem_name <- variable_names$dem_varnames[j]
-      survtime = -log(obs_matrix[i, r_name])/
-        (lambda[j]*exp(g1[j]*obs_matrix[i, "sex"] + 
-                         g2*obs_matrix[i, "U"] + 
-                         g3*obs_matrix[i, "sex"]*obs_matrix[i, "U"] +
-                         g4*obs_matrix[i, cij_slope_name] + 
-                         g5*obs_matrix[i, C_name] + 
-                         g6[j]*obs_matrix[i, dem_name]
+      survtime = -log(obs_matrix[r_name, i])/
+        (lambda[j]*exp(g1[j]*obs_matrix["sex", i] + 
+                         g2*obs_matrix["U", i] + 
+                         g3*obs_matrix["sex", i]*obs_matrix["U", i] +
+                         g4*obs_matrix[cij_slope_name, i] + 
+                         g5*obs_matrix[C_name, i] + 
+                         g6[j]*obs_matrix[dem_name, i]
                          ))
       
       survtimes[j] <- as.numeric(survtime)
@@ -24,9 +24,9 @@ survival <- function(obs_matrix){
         break
       }
     }
-    Sij[i, ] <- survtimes
+    Sij[, i] <- survtimes
   }
   Sij[Sij > 5] <- 5
-  return(list("Sij" = Sij, "survtimes" = rowSums(Sij, na.rm = TRUE)))
+  return(list("Sij" = Sij, "survtimes" = colSums(Sij, na.rm = TRUE)))
 }
 
