d3cb6089cb363f8eb09ceb4c7075e1670b66d9b5
Crystal Shaw <c.shaw@ucla.edu>
2019-06-28 19:13:37 -0700

    Changed to males as the reference for effect of log hazard on death

--- RScripts/sex-dementia_sim_parA.R
+++ RScripts/sex-dementia_sim_parA.R
@@ -67,7 +67,8 @@ b12 <- -0.005 #Effect of age on cognitive slope; Note: Everyone is the same age 
 b13 <- -0.05  #Effect of U on cognitive slope (currently age constant)
 
 #---- Parameters for Sij (survival for person i at time j) ----
-#Effect of sex on log hazard of death; chosen using calc from life_table_calcs.R 
+#Effect of sex (being female) on log hazard of death; 
+#chosen using calc from life_table_calcs.R 
 g1 <- log(c(0.5270786, 0.4693230, 0.4400211, 0.4499390, 0.4780844, 0.5407054, 
             0.6193134, 0.7189676, 0.7939100)) 
 g2 <- 0     #Effect of U on log hazard of death
