afaf1a8903a9099a8edde769fb3c2604c0081e43
Crystal Shaw <c.shaw@ucla.edu>
2019-07-01 09:26:58 -0700

    Wasn't computing dem rates just for males in the check!!!! I fixed this.

--- RScripts/dem_inc_rate_check.R
+++ RScripts/dem_inc_rate_check.R
@@ -22,6 +22,10 @@ lambda <- opt_base_haz
 num_obs = 500000
 obs <- data_gen(num_obs) %>% as.data.frame()
 
+#---- Data by sex ----
+male_data <- obs %>% filter(female == 0)
+female_data <- obs %>% filter(female == 1)
+
 #---- Compute incidence rates ----
 male_sim_inc_rates <- matrix(ncol = 9, nrow = 1)
 colnames(male_sim_inc_rates) <- variable_names$interval_ages[1:num_tests]
@@ -41,7 +45,7 @@ for(slot in 1:num_tests){
     death_this_wave <- paste0("death", (slot - 1), "-", slot)
     contributed <- paste0("contributed", (slot - 1), "-", slot)
   }
-  PY_data <- obs %>% 
+  PY_data <- male_data %>% 
     dplyr::select(death_last_wave, death_this_wave, 
                   dem_last_wave, dem_this_wave, contributed) %>% 
     filter(!! as.name(death_last_wave) == 0 & 
@@ -52,10 +56,36 @@ for(slot in 1:num_tests){
                                      sum(PY_data[, contributed])), 3)
 }
 
+female_sim_inc_rates <- matrix(ncol = 9, nrow = 1)
+colnames(female_sim_inc_rates) <- variable_names$interval_ages[1:num_tests]
+rownames(female_sim_inc_rates) <- c("")
+
+for(slot in 1:num_tests){
+  if(slot == 1){
+    dem_last_wave <- paste0("dem", (slot - 1))
+    dem_this_wave <- paste0("dem", (slot - 1), "-", slot)
+    death_last_wave <- paste0("death", (slot - 1))
+    death_this_wave <- paste0("death", (slot - 1), "-", slot)
+    contributed <- paste0("contributed", (slot - 1), "-", slot)
+  } else {
+    dem_last_wave <- paste0("dem", (slot - 2), "-", (slot - 1))
+    dem_this_wave <- paste0("dem", (slot - 1), "-", slot)
+    death_last_wave <- paste0("death", (slot - 2), "-", (slot - 1))
+    death_this_wave <- paste0("death", (slot - 1), "-", slot)
+    contributed <- paste0("contributed", (slot - 1), "-", slot)
+  }
+  PY_data <- female_data %>% 
+    dplyr::select(death_last_wave, death_this_wave, 
+                  dem_last_wave, dem_this_wave, contributed) %>% 
+    filter(!! as.name(death_last_wave) == 0 & 
+             !! as.name(dem_last_wave) == 0) 
+  
+  female_sim_inc_rates[1, slot] = round(1000*(sum(PY_data[, dem_this_wave], 
+                                                  na.rm = TRUE)/
+                                              sum(PY_data[, contributed])), 3)
+}
+
 #---- Compute survival data ----
-#---- Data by sex ----
-male_data <- obs %>% filter(female == 0)
-female_data <- obs %>% filter(female == 1)
 
 #---- Cohort size ----
 num_obs <- nrow(obs)
