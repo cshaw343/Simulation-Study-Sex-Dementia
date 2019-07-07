f30b972901ea4ceb0ad0f46e19034061707c8bda
Crystal Shaw <c.shaw@ucla.edu>
2019-07-07 15:47:38 -0700

    Update parameters for run on desktop

--- RScripts/sex-dementia_sim_parA.R
+++ RScripts/sex-dementia_sim_parA.R
@@ -8,7 +8,7 @@
 #*******************************************************************************
 
 #---- Number of simulation runs ----
-runs = 5
+runs = 1000
 
 #---- Number of observations ----
 #Multiples of 1000
