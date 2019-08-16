a33973c70e0d94599f6c03041e506e50c303eaca
Crystal Shaw <c.shaw@ucla.edu>
2019-08-15 23:03:45 -0700

    Try parallel replicate using PSOCK cluster

--- RScripts/sex-dementia_sim_run.R
+++ RScripts/sex-dementia_sim_run.R
@@ -2,13 +2,13 @@
 if (!require("pacman")) 
   install.packages("pacman", repos='http://cran.us.r-project.org')
 
-p_load("future.apply", "parallel", "here", "magrittr")
+p_load("parallel", "here", "magrittr")
 
 set.seed(20190624)
 
 #---- Source Files ----
 source(here("RScripts", 
-            "sex-dementia_sim_parA_onedemcut_nodemkill.R"))          #The parameter file
+            "sex-dementia_sim_parA_onedemcut_nodemkill.R"))  #The parameter file
 source(here("RScripts", "sex-dementia_sim_data_gen.R"))      #The data generation script
 source(here("RScripts", "sex-dementia_sim_data_analysis.R")) #The data analysis script
 source(here("RScripts", "misc_custom_functions.R"))          #Other functions needed
@@ -20,50 +20,112 @@ data_gen(500000) %>%
 
 
 #---- Running the simulation in parallel----
-#Function to run simulation in batches
-batch_runs <- function(){
-  plan(multiprocess, 
-       workers = (floor(0.5*detectCores())), 
-       gc = TRUE)
-  sim_results <- future_replicate(5, sex_dem_sim(num_obs)) %>% t() %>% 
-    as.data.frame()
-  
-  return(sim_results)
+# #Function to run simulation in batches
+# batch_runs <- function(){
+#   plan(multiprocess, 
+#        workers = (floor(0.5*detectCores())), 
+#        gc = TRUE)
+#   sim_results <- future_replicate(2, sex_dem_sim(num_obs)) %>% t() %>% 
+#     as.data.frame()
+#   
+#   return(sim_results)
+# 
+#   future:::ClusterRegistry("stop")
+# }
 
-  future:::ClusterRegistry("stop")
-}
-
-#---- Test Code ----
-if(runs%%5 != 0){
-  stop("Number of runs must be a multiple of 5.")
-}
- 
+# #---- Test Code ----
+# if(runs%%5 != 0){
+#   stop("Number of runs must be a multiple of 5.")
+# }
+#  
 gc()
 Start <- Sys.time()
 
-sim_results <- replicate(runs/5, batch_runs())
+#sim_results <- replicate(runs/5, batch_runs())
+
+cl <- makeCluster(0.5*detectCores())  
+
+# get library support needed to run the code
+clusterEvalQ(cl, {
+  if (!require("pacman")) 
+    install.packages("pacman", repos='http://cran.us.r-project.org')
+  
+  p_load("here", "magrittr")
+  
+  source(here("RScripts", 
+              "sex-dementia_sim_parA_onedemcut_nodemkill.R"))  #The parameter file
+  source(here("RScripts", "sex-dementia_sim_data_gen.R"))      #The data generation script
+  source(here("RScripts", "sex-dementia_sim_data_analysis.R")) #The data analysis script
+  source(here("RScripts", "misc_custom_functions.R"))          #Other functions needed
+})
+
+sim_results <- parSapply(cl, 1:runs, function(i) {sex_dem_sim(100000)}) %>% 
+  t() %>% as.data.frame() %>% 
+  write_csv(here("Results", "Scenario_A_no_bias", 
+                 "one_demcut_nodemkill_15_20190814.csv"))
+#stop the cluster
+stopCluster(cl)
 
 Sys.time() - Start
 
-#---- Output column names ----
-output_column_names <- rownames(sim_results)
+# #---- Output column names ----
+# output_column_names <- rownames(sim_results)
+# 
+# #---- Create results matrix ----
+# results_matrix <- 
+#   as.data.frame(matrix(NA, ncol = length(output_column_names), nrow = runs)) %>% 
+#   set_colnames(output_column_names)
+# 
+# for(i in output_column_names){
+#   results_matrix[, i] <- unlist(sim_results[i, ])
+# }
 
-#---- Create results matrix ----
-results_matrix <- 
-  as.data.frame(matrix(NA, ncol = length(output_column_names), nrow = runs)) %>% 
-  set_colnames(output_column_names)
+# write_csv(results_matrix, 
+#           here("Results", "Scenario_A_no_bias", 
+#                "one_demcut_nodemkill_1000_20190814.csv"))
 
-for(i in output_column_names){
-  results_matrix[, i] <- unlist(sim_results[i, ])
-}
-
-write_csv(results_matrix, 
-          here("Results", "Scenario_A_no_bias", 
-               "one_demcut_nodemkill_1000_20190814.csv"))
-
-
-
-
-
-
+# #---- Code testing ----
+# gc()
+# Start <- Sys.time()
+# 
+# #test <- data_gen(100000)
+# 
+# #test <- sex_dem_sim(100000)
+# 
+# # plan(multiprocess,
+# #      workers = (floor(0.5*detectCores())),
+# #      gc = TRUE)
+# # test <- future_replicate(2, replicate(1, sex_dem_sim(num_obs))) 
+# 
+# #%>% t() %>% as.data.frame()
+# 
+# cl <- makeCluster(0.5*detectCores())  
+# # get library support needed to run the code
+# clusterEvalQ(cl, {
+#   if (!require("pacman")) 
+#     install.packages("pacman", repos='http://cran.us.r-project.org')
+#   
+#   p_load("here", "magrittr")
+#   
+#   source(here("RScripts", 
+#               "sex-dementia_sim_parA_onedemcut_nodemkill.R"))  #The parameter file
+#   source(here("RScripts", "sex-dementia_sim_data_gen.R"))      #The data generation script
+#   source(here("RScripts", "sex-dementia_sim_data_analysis.R")) #The data analysis script
+#   source(here("RScripts", "misc_custom_functions.R"))          #Other functions needed
+# })
+# 
+# # put objects in place that might be needed for the code
+# #clusterExport(cl,c("myData"))
+# # Set a different seed on each member of the cluster (just in case)
+# #clusterSetRNGStream(cl)
+# #... then parallel replicate...
+# test <- parSapply(cl, 1:10, function(i) {sex_dem_sim(100000)}) %>% t() %>% 
+#   as.data.frame()
+# #stop the cluster
+# stopCluster(cl)
+# 
+# #test <- replicate(4, sex_dem_sim(num_obs)) %>% t() %>% as.data.frame()
+# 
+# Sys.time() - Start
+# 
 
