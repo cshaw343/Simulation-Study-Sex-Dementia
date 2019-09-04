b55c934aaefc05e021ae3becd14961d6df93d347
Crystal Shaw <c.shaw@ucla.edu>
2019-09-03 22:21:47 -0700

    Censor data using function rather than large matrices

---
+++ RScripts/quadratic_model/survival_censor.R
@@ -0,0 +1,12 @@
+survival_censor <- function(obs){
+  max_Cij = length(variable_names$Cij_varnames)
+  max_dem = length(variable_names$dem_varnames)
+  for(i in 1:ncol(obs)){
+    if(obs["study_death", i] == 1){
+      last_wave <- floor(obs["survtime", i]/5)
+      obs[variable_names$Cij_varnames[(last_wave + 2):max_Cij], i] <- NA
+      obs[variable_names$dem_varnames[(last_wave + 2):max_dem], i] <- NA
+    }
+  }
+  return(obs)
+}
