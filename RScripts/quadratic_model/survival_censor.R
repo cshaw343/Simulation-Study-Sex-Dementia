6169aefaa6b07c7f9f186f923ed0a2ac1c00ad25
Crystal Shaw <c.shaw@ucla.edu>
2019-09-03 22:37:34 -0700

    Spacing edit

--- RScripts/quadratic_model/survival_censor.R
+++ RScripts/quadratic_model/survival_censor.R
@@ -1,6 +1,7 @@
 survival_censor <- function(obs){
   max_Cij = length(variable_names$Cij_varnames)
   max_dem = length(variable_names$dem_varnames)
+  
   for(i in 1:ncol(obs)){
     if(obs["study_death", i] == 1){
       last_wave <- floor(obs["survtime", i]/5)
@@ -8,5 +9,6 @@ survival_censor <- function(obs){
       obs[variable_names$dem_varnames[(last_wave + 2):max_dem], i] <- NA
     }
   }
+  
   return(obs)
 }
