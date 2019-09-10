7a4642e03c8892b36249c4fb61f7c87d8c14c3f0
Crystal Shaw <c.shaw@ucla.edu>
2019-09-10 15:21:06 -0700

    Chose different value of b10 based on test different parabola shapes and hand-calibration

--- RScripts/quadratic_model/sex-dementia_sim_parA_onedemcut_nodemkill_maleAD_quad.R
+++ RScripts/quadratic_model/sex-dementia_sim_parA_onedemcut_nodemkill_maleAD_quad.R
@@ -54,7 +54,7 @@ b02 <- 0      #Effect of age on cognitive intercept; Note: Everyone is the same 
 b03 <- -0.1   #Effect of U (unmeasured/underlying variable) on cognitive intercept (taken from Marden et. al. 2017)
 
 
-b10 <- 0.05   #Cognitive linear term for males (taken from quad fit to linear model)                
+b10 <- 0.025   #Cognitive linear term for males (taken from quad fit to linear model)                
 b11 <- 0      #Effect of female on cognitive linear term
 b12 <- 0      #Effect of age on cognitive linear term; Note: Everyone is the same age so there is no age effect
 b13 <- 0      #Effect of U on cognitive linear term 
