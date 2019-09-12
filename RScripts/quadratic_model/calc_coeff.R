3c8ebd081d77185a3e3b612b45ae267daad706f7
Crystal Shaw <c.shaw@ucla.edu>
2019-09-12 11:34:39 -0700

    Fixed typo

--- RScripts/quadratic_model/calc_coeff.R
+++ RScripts/quadratic_model/calc_coeff.R
@@ -3,7 +3,7 @@ calc_coeff <- function(obs){
     b03*obs[, "U"] + obs[, "eps_ij"]
   a1 <- b10 + obs[, "z_1i"] + b11*obs[, "female"] + b12*obs[, "age0_c50"] + 
     b13*obs[, "U"] 
-  a2 <- b20 + obs[, "z_2i"] + b12*obs[, "female"] + b22*obs[, "age0_c50"] + 
+  a2 <- b20 + obs[, "z_2i"] + b21*obs[, "female"] + b22*obs[, "age0_c50"] + 
     b23*obs[, "U"] 
   
   return(cbind(a0, a1, a2))
