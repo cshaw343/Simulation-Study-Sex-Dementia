f00eb2247087c3cfe99c4e83e8902d5cba9a4a29
Crystal Shaw <c.shaw@ucla.edu>
2019-09-15 12:05:06 -0700

    Updated with parameters from hand optimization so far

--- RScripts/quadratic_model/sex-dementia_sim_parA_onedemcut_nodemkill_maleAD_quad.R
+++ RScripts/quadratic_model/sex-dementia_sim_parA_onedemcut_nodemkill_maleAD_quad.R
@@ -33,13 +33,13 @@ pfemale <- 0.51
 
 #---- Variances and correlations ----
 #Calculated based on the work in test_quadratic_trajectories.R
-cij_var0 <- 0.05    #Variance of random cognitive intercept
-cij_var1 <- 0.11    #Variance of random linear term
-cij_var2 <- 0.001   #Variance of random quadratic term (use tiny value b/c calcs give 0)
+cij_var0 <- 0.05       #Variance of random cognitive intercept
+cij_var1 <- 0.00009    #Variance of random linear term
+cij_var2 <- 0.000005   #Variance of random quadratic term (use tiny value b/c calcs give 0)
 
-cij_cov01 <- -0.07     #Covariance between random intercept and random linear term
-cij_cov12 <- 0.002   #Covariance between random linear and random quadratic term
-cij_cov02 <- 0.001    #Covariance between random intercept and random quadratic term (use tiny value b/c calcs give 0)
+cij_cov01 <- 0          #Covariance between random intercept and random linear term
+cij_cov12 <- -0.000001  #Covariance between random linear and random quadratic term
+cij_cov02 <- 0.0001     #Covariance between random intercept and random quadratic term (use tiny value b/c calcs give 0)
 
 cij_var3 <- 1     #Variance of noise for Cij (cognitive function for person i at time j)
 
@@ -54,15 +54,15 @@ b02 <- 0      #Effect of age on cognitive intercept; Note: Everyone is the same 
 b03 <- -0.1   #Effect of U (unmeasured/underlying variable) on cognitive intercept (taken from Marden et. al. 2017)
 
 
-b10 <- 4       #Cognitive linear term for males (taken from quad fit to linear model)                
+b10 <- 0.1     #Cognitive linear term for males (taken from quad fit to linear model)                
 b11 <- 0       #Effect of female on cognitive linear term
 b12 <- 0       #Effect of age on cognitive linear term; Note: Everyone is the same age so there is no age effect
 b13 <- 0       #Effect of U on cognitive linear term 
 
-b20 <- -0.12  #Cognitive quadratic term for males (taken from quad fit to linear model)
-b21 <- 0      #Effect of female on cognitive quadratic term
-b22 <- 0      #Effect of age on cognitive quadratic term; Note: Everyone is the same age so there is no age effect 
-b23 <- 0      #Effect of U on cognitive quadratic term
+b20 <- -0.00725  #Cognitive quadratic term for males (taken from quad fit to linear model)
+b21 <- 0         #Effect of female on cognitive quadratic term
+b22 <- 0         #Effect of age on cognitive quadratic term; Note: Everyone is the same age so there is no age effect 
+b23 <- 0         #Effect of U on cognitive quadratic term
 
 #---- Parameters for Sij (survival for person i at time j) ----
 #Effect of sex (being female) on log hazard of death; 
@@ -83,5 +83,5 @@ lambda <- c(0.0142, 0.0202, 0.0289, 0.0436, 0.0676, 0.1001, 0.1532, 0.2300,
 
 #---- Dementia Cut Point ----
 #Based on slopes_dem-cut_search.R script (results from 20190202)
-dem_cut <- -4
+dem_cut <- -6.5
 
