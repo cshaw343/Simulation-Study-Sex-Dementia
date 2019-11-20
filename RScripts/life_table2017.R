ab4a3aef7e6166fbba917947bd1ac5fc9684dbd8
Crystal Shaw <c.shaw@ucla.edu>
2019-11-19 17:59:35 -0800

    Added misc. custom functions into this script (this is the only one that uses these functions)

--- RScripts/life_table2017.R
+++ RScripts/life_table2017.R
@@ -15,6 +15,27 @@ p_load("tidyverse", "here")
 #---- Source Files ----
 source(here("RScripts", "misc_custom_functions.R"))
 
+#---- Conditional probabilities function ----
+cond_prob <- function(x){
+  probs <- vector(length = length(x))
+  for(i in 2:length(probs)){
+    probs[i] = x[i]/x[i - 1]
+  }
+  probs[1] <- 1
+  return(probs)
+}
+
+#---- Recursive subtraction function ----
+sub_recurse <- function(x){
+  diffs <- vector(length = length(x))
+  for(i in 2:length(diffs)){
+    diffs[i] = x[i] - x[i - 1]
+  }
+  diffs[1] <- x[1]
+  return(diffs)
+}
+
+
 #---- Hazard Function ----
 haz <- function(age, logprobs){
   HZ <- vector(length = length(age))
