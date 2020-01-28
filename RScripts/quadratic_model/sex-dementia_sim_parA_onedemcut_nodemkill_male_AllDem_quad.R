cd1eafa108951fac6ae8c9f93275f44b393a5b29
cshawsome <c.shaw@ucla.edu>
2020-01-27 17:52:18 -0800

    Put in placeholder for cumulative incidence of mortality

--- RScripts/quadratic_model/sex-dementia_sim_parA_onedemcut_nodemkill_male_AllDem_quad.R
+++ RScripts/quadratic_model/sex-dementia_sim_parA_onedemcut_nodemkill_male_AllDem_quad.R
@@ -1,6 +1,6 @@
 #*******************************************************************************
 # Simulation Senario A
-# 25% cumulative incidence of mortality
+# XXXX% cumulative incidence of mortality
 # Exposure = male gender
 # Based on US lifetables, sample is 51% F / 49% M at age 50
 # Remaining LE at age 50:  XXXXX years for F and XXXXX years for M
