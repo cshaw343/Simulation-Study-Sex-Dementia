62db9e39b37266cebdbbb4db5a7aeeb312774c3e
cshawsome <c.shaw@ucla.edu>
2020-02-07 13:54:54 -0800

    Update column names

--- RScripts/format_plot_data.R
+++ RScripts/format_plot_data.R
@@ -6,13 +6,18 @@ format_plot_data <- function(sample, scenario){
     mutate("Scenario" = scenario) %>%
     dplyr::select(-one_of("female")) %>% 
     dplyr::select("U", "death0", everything()) %>% 
-    set_colnames(c("U", seq(1:10), "Sex/Gender", "Scenario")) %>% 
-    gather(-contains(c("U", "Sex/Gender", "Scenario")), key = "Visit", 
+    set_colnames(c("U", "Baseline", "[50-55)", "[55-60)","[60-65)", 
+                   "[65-70)", "[70-75)","[75-80)", 
+                   "[80-85)", "[85-90)","[90-95)", 
+                   "Sex/Gender", "Scenario")) %>% 
+    gather(-contains(c("U", "Sex/Gender", "Scenario")), key = "Age Band", 
            value = "death_indicator") %>%
     filter(death_indicator == 0) %>% 
     mutate_at("Sex/Gender", as.factor) %>% 
-    mutate_at("Visit", as.factor)
-  plot_data$Visit <- fct_relevel(plot_data$Visit, "10", after = Inf)
+    mutate_at("Age Band", as.factor)
+  plot_data$`Age Band` <- relevel(plot_data$`Age Band`, "Baseline")
+  plot_data$`Age Band` <- fct_relevel(plot_data$`Age Band`, 
+                                      rev(levels(plot_data$`Age Band`)))
   
   return(plot_data)
 }
