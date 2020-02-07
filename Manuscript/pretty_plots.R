353e1b6fa8cabc64807897d9bf9e3b05ebc4dcdf
cshawsome <c.shaw@ucla.edu>
2020-02-06 23:07:06 -0800

    Load ACT data

--- Manuscript/pretty_plots.R
+++ Manuscript/pretty_plots.R
@@ -1,3 +1,38 @@
+#---- Package Loading, Options, and Seed ----
+if (!require("pacman")) 
+  install.packages("pacman", repos='http://cran.us.r-project.org')
+
+p_load("here", "tidyverse")
+
+#---- Load ACT data ----
+source(here("RScripts", "dementia_incidence_ACT.R"))
+
+#---- Load results data ----
+results_A <- read_csv(here(
+  "Results", "quadratic_model", "Scenario_A_no_bias", 
+  "sim_A_male_AllDem_1000_20191125.csv")) %>% 
+  results_65_plus()
+
+results_B1 <- read_csv(here(
+  "Results", "quadratic_model", "Scenario_B", 
+  "sim_B_male_AllDem_1000_20191128.csv")) %>% 
+  results_65_plus()
+
+
+#---- Calculate mean and sd of results ----
+mean_results_A <- results_A %>% colMeans()
+mean_results_B1 <- results_B1 %>% colMeans()
 
 
 #---- Figure 2 ----
+IRR_table <- 
+  tibble("ACT" = " ", 
+         "IRR (Women:Men)_ACT" = c(rep("NA", 3), 
+                                   round(ACT_inc_rates$`All_Dem_IRR_F:M`, 3)),
+         "Simulation Scenario A" = " ", 
+         "IRR (Women:Men)" = exp(
+           mean_sim_results[na.omit(variable_names$logIRR_varnames)])) %>% 
+  t() %>% 
+  set_colnames(age_labels$formatted_age_intervals) %>% 
+  set_rownames(c("ACT", "IRR (Women:Men)", "Simulation Scenario A", 
+                 "IRR (Women:Men)"))
