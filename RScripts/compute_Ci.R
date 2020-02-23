6371b6ae4f48e228da104001a985a20954579679
cshawsome <c.shaw@ucla.edu>
2020-02-23 13:12:42 -0800

    Update notation to match manuscript

--- RScripts/compute_Ci.R
+++ RScripts/compute_Ci.R
@@ -1,10 +1,10 @@
-compute_Cij <- function(obs){
-  Cij <- matrix(nrow = nrow(obs), ncol = length(visit_times))
-  for(i in 1:ncol(Cij)){
+compute_Ci <- function(obs){
+  Ci <- matrix(nrow = nrow(obs), ncol = length(visit_times))
+  for(i in 1:ncol(Ci)){
     centered_age <- visit_times[i]
-    Cij[, i] <- obs[, "a0"] + obs[, "a1"]*centered_age + 
+    Ci[, i] <- obs[, "a0"] + obs[, "a1"]*centered_age + 
       obs[, "a2"]*centered_age^2
   }
   
-  return(Cij)
+  return(Ci)
 }
