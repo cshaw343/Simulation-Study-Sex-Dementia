b5d1f9b983823a6a4468b8629ba12900e66fd248
cshawsome <c.shaw@ucla.edu>
2020-02-23 13:40:56 -0800

    Update notation to match manuscript

--- RScripts/last_Ci.R
+++ RScripts/last_Ci.R
@@ -1,15 +1,15 @@
-last_Cij <- function(obs){
-  last_Cij <- vector(length = ncol(obs))
+last_Ci <- function(obs){
+  last_Ci <- vector(length = ncol(obs))
   
-  for(i in 1:length(last_Cij)){
+  for(i in 1:length(last_Ci)){
     if(obs["study_death", i] == 1){
       survtime <- obs["survtime", i]
-      last_Cij[i] <- 
+      last_Ci[i] <- 
         obs["a0", i] + obs["a1", i]*survtime + obs["a2", i]*survtime^2
     } else{
-      last_Cij[i] <- NA
+      last_Ci[i] <- NA
     }
   }
   
-  return(last_Cij)
+  return(last_Ci)
 }
