183daa07db15b1b047ada0ff0d380a2581a2ee8e
cshawsome <c.shaw@ucla.edu>
2020-02-23 12:35:37 -0800

    Change exposure to "female gender" in all header text for scenarios

--- RScripts/scenario_A_pars.R
+++ RScripts/scenario_A_pars.R
@@ -1,7 +1,7 @@
 #*******************************************************************************
 # Simulation Senario A
 # 99.1% cumulative incidence of mortality
-# Exposure = male gender
+# Exposure = female gender
 # Based on US lifetables, sample is 51% F / 49% M at age 50
 # Remaining LE (median) at age 50:  23.5 years for F and 22.5 years for M
 #*******************************************************************************
@@ -16,7 +16,7 @@ int_time <- 5
 #Number of assessments
 num_tests <- 9
 
-#Resulting visit times
+#Resulting visit times (years from baseline)
 visit_times <- seq(from = 0, to = int_time*num_tests, by = int_time)
 
 #---- Prevalance of exposure (female) ----
--- RScripts/scenario_B1_pars.R
+++ RScripts/scenario_B1_pars.R
@@ -1,7 +1,7 @@
 #*******************************************************************************
 # Simulation Senario B1
 # 99.1% cumulative incidence of mortality
-# Exposure = male gender
+# Exposure = female gender
 # Based on US lifetables, sample is 51% F / 49% M at age 50
 # Remaining LE (median) at age 50:  23.5 years for F and 22.5 years for M
 #*******************************************************************************
