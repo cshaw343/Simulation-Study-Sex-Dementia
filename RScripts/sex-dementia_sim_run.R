1c5c8d57aee476422f07e224da9aa326f5d88337
cshawsome <c.shaw@ucla.edu>
2020-02-23 15:03:08 -0800

    Add options to specify file names Switch to future_replicate for parallel runs

--- RScripts/sex-dementia_sim_run.R
+++ RScripts/sex-dementia_sim_run.R
@@ -13,51 +13,38 @@ source(here("RScripts", "var_names.R"))
 source(here("RScripts", "data_gen.R"))         #The data generation script
 source(here("RScripts", "data_analysis.R"))    #The data analysis script
 
+#---- Set filenames for output ----
+#Paths are taken care of with the here package
+#Specify string for filename
+one_cohort_output <- "one_cohort_A"
+simulation_output <- "sim_run_A.csv"
+
 #---- Generating one cohort ----
 #Use this to get one cohort for cohort specific checks and plots 
 #(like the U plots)
 #Set the desired number of people in the data_gen function
 #Set the desired file name in the here function where it says "filename" 
 data_gen(num_obs = 1000) %>%
-  saveRDS(here("Data", "filename"))
+  saveRDS(here("Data", one_cohort_output))
 
 #---- Running the full simulation ----
 #Simulation settings
-runs = 2         #Number of simulation runs
-num_obs <- 1000   #Size of each simulated cohort
+runs = 2              #Number of simulation runs
+num_obs <- 100000     #Size of each simulated cohort
 
-gc()                #Clear the environment of unecessary junk
-Start <- Sys.time()
+gc()                  #Clear the environment of unecessary junk
+start <- Sys.time()   #Start timing the code
 
-start <- Sys.time()
-plan(multiprocess, workers = 0.5*availableCores())
+#Run in parallel using half of the available cores
+plan(multiprocess, workers = 0.5*availableCores()) 
 
-cl <- makeCluster(0.5*detectCores())  
+sim_results <- future_replicate(runs, sex_dem_sim(num_obs)) %>% t() %>% 
+  as.data.frame() 
 
-# get library support needed to run the code
-clusterEvalQ(cl, {
-  if (!require("pacman")) 
-    install.packages("pacman", repos='http://cran.us.r-project.org')
-  
-  p_load("here", "magrittr")
-  
-  source(
-    here("RScripts", "quadratic_model", 
-         "sex-dementia_sim_parC_onedemcut_nodemkill_male_AllDem_quad.R"))  #The parameter file
-  source(here("RScripts", "quadratic_model", "variable_names_quad.R"))
-  source(here("RScripts", "quadratic_model", 
-              "sex-dementia_sim_data_gen_quad.R"))      #The data generation script
-  source(here("RScripts", "sex-dementia_sim_data_analysis.R")) #The data analysis script
-})
-
-sim_results <- parSapply(cl, 1:runs, function(i) {sex_dem_sim(num_obs)}) %>% 
-  t() %>% as.data.frame() 
-
-write_csv(sim_results, 
-          here("Results", "quadratic_model", "Scenario_C", 
-               paste0("sim_C_male_AllDem_", runs, "_20200212.csv")))
+write_csv(sim_results, here("Results", simulation_output))
 
 #stop the cluster
-stopCluster(cl)
+plan(sequential)
 
-Sys.time() - Start
+#How long did it take
+Sys.time() - start
