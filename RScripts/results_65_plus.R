37e4042489064a75a17da8116f3929064589cdb1
cshawsome <c.shaw@ucla.edu>
2020-02-23 17:11:49 -0800

    Update notation to match manuscript

--- RScripts/results_65_plus.R
+++ RScripts/results_65_plus.R
@@ -6,13 +6,13 @@ results_65_plus <- function(results){
     dplyr::select(-one_of(variable_names$logIRR_95CI_Coverage_varnames[1:3])) %>% 
     dplyr::select(-one_of(variable_names$logIRR_SE_varnames[1:3])) %>% 
     dplyr::select(-one_of(variable_names$prop_dem_random_by_age[1:3])) %>%
-    dplyr::select(-one_of(variable_names$prop_dem_Cij_by_age[1:3])) %>%
+    dplyr::select(-one_of(variable_names$prop_dem_Ci_by_age[1:3])) %>%
     dplyr::select(-one_of(variable_names$prop_dem_both_by_age[1:3])) %>%
     dplyr::select(-one_of(variable_names$prop_dem_random_W_by_age[1:3])) %>%
-    dplyr::select(-one_of(variable_names$prop_dem_Cij_W_by_age[1:3])) %>%
+    dplyr::select(-one_of(variable_names$prop_dem_Ci_W_by_age[1:3])) %>%
     dplyr::select(-one_of(variable_names$prop_dem_both_W_by_age[1:3])) %>%
     dplyr::select(-one_of(variable_names$prop_dem_random_M_by_age[1:3])) %>%
-    dplyr::select(-one_of(variable_names$prop_dem_Cij_M_by_age[1:3])) %>%
+    dplyr::select(-one_of(variable_names$prop_dem_Ci_M_by_age[1:3])) %>%
     dplyr::select(-one_of(variable_names$prop_dem_both_M_by_age[1:3])) %>%
     dplyr::select(-one_of(variable_names_1year$logIRR_varnames[1:15])) %>%
     dplyr::select(
